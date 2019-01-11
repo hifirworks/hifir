c5118bd3c329e9db13995ff06b49bcbcc0eb8469
Qiao Chen <qiao.chen@stonybrook.edu>
2019-01-10 21:20:08 -0500

    Finish testing about Array

--- tests/test_array.cpp
+++ tests/test_array.cpp
@@ -11,11 +11,13 @@
 #include "psmilu_Array.hpp"
 
 #include <gtest/gtest.h>
+#include <list>
+#include <utility>
 #include <vector>
 
 using namespace psmilu;
 
-TEST(Array_api, test1) {
+TEST(Array_api, test_core) {
   Array<double> v;
   ASSERT_EQ(v.status(), DATA_UNDEF);
   v.resize(100);
@@ -40,10 +42,47 @@ TEST(Array_api, test1) {
   for (const auto i : v) ASSERT_EQ(i, -1.0);
 }
 
-TEST(Array_api, test2) {
+TEST(Array_api, test_wrap) {
   std::vector<int> _v(100);
   Array<int>       v(100u, _v.data(), true);
   ASSERT_EQ(v.status(), DATA_WRAP);
   for (auto &i : v) i = -100.0;
   for (const auto i : _v) ASSERT_EQ(i, -100.0);
+}
+
+TEST(Array_api, test_pushback) {
+  Array<long> v;
+  v.push_back(1l);
+  ASSERT_EQ(v.front(), 1l);
+  ASSERT_EQ(v.back(), 1l);
+  for (int i = 0; i < 9; ++i) v.push_back(v.front() + i + 1);
+  {
+    long j(1l);
+    for (const auto i : v) ASSERT_EQ(i, j++);
+  }
+  const auto        n = v.size();
+  std::vector<long> b(v.cbegin(), v.cend());
+  v.push_back(b.cbegin(), b.cend());
+  ASSERT_EQ(v.size(), n * 2u);
+  for (long i = 0l; i < (long)v.size(); ++i) ASSERT_EQ((i % n) + 1l, v[i]);
+  // test list
+  std::list<long> c(b.cbegin(), b.cend());
+  v.push_back(c.cbegin(), c.cend());
+  ASSERT_EQ(v.size(), n * 3u);
+  for (long i = 0l; i < (long)v.size(); ++i) ASSERT_EQ((i % n) + 1l, v[i]);
+}
+
+TEST(Array_api, test_move) {
+  Array<float> v1(100);
+  Array<float> v2(std::move(v1));
+  ASSERT_EQ(v1.status(), DATA_UNDEF);
+  ASSERT_EQ(v1.size(), 0u);
+  ASSERT_EQ(v2.size(), 100u);
+  ASSERT_EQ(v2.status(), DATA_OWN);
+  Array<float> v3;
+  v3 = std::move(v2);
+  ASSERT_EQ(v2.status(), DATA_UNDEF);
+  ASSERT_EQ(v2.size(), 0u);
+  ASSERT_EQ(v3.size(), 100u);
+  ASSERT_EQ(v3.status(), DATA_OWN);
 }
