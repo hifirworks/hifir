0a31f4be8f80d47a376875f19b8da3acfc355108
Qiao Chen <qiao.chen@stonybrook.edu>
2019-01-13 19:14:40 -0500

    added CRS test

--- tests/test_crs.cpp
+++ tests/test_crs.cpp
@@ -48,6 +48,8 @@ static std::vector<std::vector<T>> create_mat(const int nrows,
       for (decltype(i) j = 0u; j < m; ++j) ASSERT_EQ(mat1[i][j], mat2[i][j]); \
   } while (false)
 
+#define get_a_rand (std::rand() % 100000) / 100000.0
+
 using namespace psmilu;
 
 TEST(CRS_api, test_core_c) {
@@ -64,7 +66,7 @@ TEST(CRS_api, test_core_c) {
   if (nrows < 5u) nrows = 5u;
   if (ncols < 5u) ncols = 5u;
   crs1.resize(nrows, ncols);
-#define get_a_rand (std::rand() % 100000) / 100000.0
+
   auto                          mat_ref = create_mat<double>(nrows, ncols);
   std::vector<std::vector<int>> nnz_list(nrows);
   std::vector<bool>             col_tags(ncols, false);
@@ -85,6 +87,65 @@ TEST(CRS_api, test_core_c) {
   crs1.begin_assemble_rows();
   for (s_t i = 0u; i < nrows; ++i) {
     for (const auto col : nnz_list[i]) mat_ref[i][col] = get_a_rand;
+    crs1.push_back_row(i, nnz_list[i].cbegin(), nnz_list[i].cend(), mat_ref[i]);
+    ASSERT_EQ(crs1.nnz_in_row(i), nnz_list[i].size());
+  }
+  crs1.end_assemble_rows();
+  const auto mat2 = convert_crs2dense(crs1);
+  COMPARE_MATS(mat_ref, mat2);
+  // test return
+  const auto return_crs1 = [&]() -> crs_t { return crs1; };
+  auto       crs2        = return_crs1();
+  ASSERT_EQ(crs1.col_ind().data(), crs2.col_ind().data());
+  ASSERT_EQ(crs2.nrows(), nrows);
+  ASSERT_EQ(crs2.ncols(), ncols);
+  // test wrap
+  std::vector<int> rowptr(crs1.row_start().cbegin(), crs1.row_start().cend()),
+      colind(crs1.col_ind().cbegin(), crs1.col_ind().cend());
+  std::vector<double> vals(crs1.vals().cbegin(), crs1.vals().cend());
+  crs_t crs3(nrows, ncols, rowptr.data(), colind.data(), vals.data(), true);
+  ASSERT_EQ(crs3.status(), DATA_WRAP);
+  ASSERT_EQ(crs3.vals().data(), vals.data());
+  const auto mat3 = convert_crs2dense(crs3);
+  COMPARE_MATS(mat3, mat2);
+}
+
+TEST(CRS_api, test_core_fortran) {
+  typedef CRS<double, int, true> crs_t;
+  typedef crs_t::size_type       s_t;
+  crs_t                          crs1;
+  ASSERT_EQ(crs1.nrows(), 0u);
+  ASSERT_EQ(crs1.ncols(), 0u);
+  crs1.resize(10, 20);
+  ASSERT_EQ(crs1.nrows(), 10u);
+  ASSERT_EQ(crs1.ncols(), 20u);
+  std::srand(std::time(0));
+  s_t nrows(std::rand() % 50), ncols(std::rand() % 50);
+  if (nrows < 5u) nrows = 5u;
+  if (ncols < 5u) ncols = 5u;
+  crs1.resize(nrows, ncols);
+
+  auto                          mat_ref = create_mat<double>(nrows, ncols);
+  std::vector<std::vector<int>> nnz_list(nrows);
+  std::vector<bool>             col_tags(ncols, false);
+  for (s_t i = 0u; i < nrows; ++i) {
+    const int nnz    = std::rand() % ncols;
+    int       counts = 0;
+    for (;;) {
+      if (counts == nnz) break;
+      const int idx = std::rand() % ncols;
+      if (col_tags[idx]) continue;
+      ++counts;
+      nnz_list[i].push_back(idx + 1);  // to fortran
+      col_tags[idx] = true;
+    }
+    for (const auto j : nnz_list[i]) col_tags[j - 1] = false;  // back to c
+  }
+  // construct sparse matrix
+  crs1.begin_assemble_rows();
+  for (s_t i = 0u; i < nrows; ++i) {
+    for (const auto col : nnz_list[i])
+      mat_ref[i][col - 1] = get_a_rand;  // to c
     crs1.push_back_row(i, nnz_list[i].cbegin(), nnz_list[i].cend(), mat_ref[i]);
     ASSERT_EQ(crs1.nnz_in_row(i), nnz_list[i].size());
   }
