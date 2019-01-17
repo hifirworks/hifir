9ebf515d28453de2add467ad6ebbb1642a5228dd
Qiao Chen <qiao.chen@stonybrook.edu>
2019-01-16 21:17:20 -0500

    added fortran index testing for crs, fixed minor typos

--- tests/test_augcrs_interchange.cpp
+++ tests/test_augcrs_interchange.cpp
@@ -77,7 +77,7 @@ TEST(AUG_CRS_SWAP, c) {
     load_dense_col(m, mat1, buf1);       // load dense
     load_aug_crs_col(m, aug_crs, buf2);  // load sparse
     for (int j = 0; j < i; ++j)
-      ASSERT_EQ(buf1[j], buf2[j]) << "col " << k << " did not match!\n";
+      ASSERT_EQ(buf1[j], buf2[j]) << "col " << m << " did not match!\n";
   }
   aug_crs.end_assemble_rows();
   // test static interchanges
@@ -93,6 +93,78 @@ TEST(AUG_CRS_SWAP, c) {
     load_dense_col(m, mat1, buf1);       // load dense
     load_aug_crs_col(m, aug_crs, buf2);  // load sparse
     for (int j = 0; j < i; ++j)
+      ASSERT_EQ(buf1[j], buf2[j]) << "col " << m << " did not match!\n";
+  }
+}
+
+typedef AugCRS<CRS<double, int, true>> aug_crs_fortran_t;
+
+TEST(AUG_CRS_SWAP, fortran) {
+  aug_crs_fortran_t aug_crs;
+  int               nrows = i_rand(), ncols = i_rand();
+  if (nrows < 5) nrows = 5;
+  if (ncols < 5) ncols = 5;
+  aug_crs.resize(nrows, ncols);
+  ASSERT_EQ(aug_crs.nrows(), nrows);
+  ASSERT_EQ(aug_crs.ncols(), ncols);
+  auto                          mat1 = create_mat<double>(nrows, ncols);
+  std::vector<std::vector<int>> col_inds(nrows);
+  std::vector<bool>             tags(ncols, false);
+  for (int i = 0; i < nrows; ++i) {
+    int nnz = i_rand() % ncols;
+    if (!nnz) nnz = (i_rand() % 2) ? 0 : ncols;
+    int counts = 0, guard = 0;
+    for (;;) {
+      if (counts == nnz || guard > 2 * nnz) break;
+      const int idx = i_rand() % ncols;
+      ++guard;
+      if (tags[idx]) continue;
+      ++counts;
+      tags[idx] = true;
+      col_inds[i].push_back(idx + 1);
+    }
+    std::sort(col_inds[i].begin(), col_inds[i].end());
+    for (const auto j : col_inds[i]) tags[j - 1] = false;
+  }
+  // test incremental column swapping
+  int nnz = 0;
+  for (const auto &l : col_inds) nnz += l.size();
+  aug_crs.reserve(nnz);
+  std::vector<double> buf1(nrows), buf2(nrows);
+  aug_crs.begin_assemble_rows();
+  for (int i = 0; i < nrows; ++i) {
+    const auto &col_ind = col_inds[i];
+    for (const auto col : col_ind) mat1[i][col - 1] = r_rand();
+    aug_crs.push_back_row(i, col_ind.cbegin(), col_ind.cend(), mat1[i]);
+    ASSERT_EQ(aug_crs.nnz_in_row(i), col_ind.size());
+    // swap here
+    GET_RANDOM_K_M;
+    std::cout << "interchange columns " << k << " and " << m << '\n';
+    aug_crs.interchange_cols(k, m);
+    interchange_dense_cols(mat1, k, m);
+    load_dense_col(k, mat1, buf1);       // load dense
+    load_aug_crs_col(k, aug_crs, buf2);  // load sparse
+    for (int j = 0; j < i; ++j)
       ASSERT_EQ(buf1[j], buf2[j]) << "col " << k << " did not match!\n";
+    load_dense_col(m, mat1, buf1);       // load dense
+    load_aug_crs_col(m, aug_crs, buf2);  // load sparse
+    for (int j = 0; j < i; ++j)
+      ASSERT_EQ(buf1[j], buf2[j]) << "col " << m << " did not match!\n";
+  }
+  aug_crs.end_assemble_rows();
+  // test static interchanges
+  for (int i = 0; i < std::min(nrows, 20); ++i) {
+    GET_RANDOM_K_M;
+    std::cout << "interchange columns " << k << " and " << m << '\n';
+    aug_crs.interchange_cols(k, m);
+    interchange_dense_cols(mat1, k, m);
+    load_dense_col(k, mat1, buf1);       // load dense
+    load_aug_crs_col(k, aug_crs, buf2);  // load sparse
+    for (int j = 0; j < i; ++j)
+      ASSERT_EQ(buf1[j], buf2[j]) << "col " << k << " did not match!\n";
+    load_dense_col(m, mat1, buf1);       // load dense
+    load_aug_crs_col(m, aug_crs, buf2);  // load sparse
+    for (int j = 0; j < i; ++j)
+      ASSERT_EQ(buf1[j], buf2[j]) << "col " << m << " did not match!\n";
   }
 }
