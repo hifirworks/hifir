d644a754504ba29dd2b49e380ebce1b4420f9dd2
Qiao Chen <qiao.chen@stonybrook.edu>
2019-01-22 21:07:30 -0500

    need to work on update start indices

--- src/psmilu_Crout.hpp
+++ src/psmilu_Crout.hpp
@@ -76,6 +76,7 @@ class Crout {
 
     // first load the A
     _load_A2l(ccs_A, p, qk, l);
+    std::cout << "load-L done!" << std::endl;
     // if not the first step
     // compute -L(k+1:n,1:k-1)d(1:k-1,1:k-1)U(1:k-1,k)
     if (_step) {
@@ -89,10 +90,10 @@ class Crout {
         // get the row index (C-based)
         // NOTE the row index is that of the column in L
         const index_type r_idx = U.row_idx(aug_id);
-        psmilu_assert(r_idx < d.size(), "%zd exceeds the diagonal vector size",
-                      r_idx);
-        psmilu_assert(r_idx < L_start.size(), "%zd exceeds the L_start size",
-                      r_idx);
+        psmilu_assert((size_type)r_idx < d.size(),
+                      "%zd exceeds the diagonal vector size", (size_type)r_idx);
+        psmilu_assert((size_type)r_idx < L_start.size(),
+                      "%zd exceeds the L_start size", (size_type)r_idx);
         // get the value position
         // NOTE this needs to be changed when we discard val_pos impl (U_start)
         // compute D*U first for this entry
@@ -101,6 +102,7 @@ class Crout {
         auto L_v_itr  = L_v_first + L_start[r_idx];
         auto L_i_itr  = L_i_first + L_start[r_idx],
              L_i_last = L.row_ind_cend(r_idx);
+             std::cout << "here " << L_start[r_idx] << std::endl;
         // for this local range
         for (; L_i_itr != L_i_last; ++L_i_itr, ++L_v_itr) {
           // convert to c index
@@ -139,6 +141,7 @@ class Crout {
 
     // first load the A
     _load_A2ut<IsSymm>(s, crs_A, t, q, pk, m, ut);
+    std::cout << "step=" << _step << '\n';
     // if not the first step
     // compute -L(k,1:k-1)d(1:k-1,1:k-1)U(1:k-1,k+1:n)
     if (_step) {
@@ -153,10 +156,11 @@ class Crout {
         // get the column index (C-based)
         // NOTE the column index is that of the row in U
         const index_type c_idx = L.col_idx(aug_id);
-        psmilu_assert(c_idx < d.size(), "%zd exceeds the diagonal vector size",
-                      c_idx);
-        psmilu_assert(c_idx < U_start.size(), "%zd exceeds the U_start size",
-                      c_idx);
+        std::cout << _step << ':' << c_idx << '\n';
+        psmilu_assert((size_type)c_idx < d.size(),
+                      "%zd exceeds the diagonal vector size", (size_type)c_idx);
+        psmilu_assert((size_type)c_idx < U_start.size(),
+                      "%zd exceeds the U_start size", (size_type)c_idx);
         // NOTE once we drop val-pos impl, change this accordingly (L_start)
         // compute L*D
         const auto ld = *(L_v_first + L.val_pos_idx(aug_id)) * d[c_idx];
@@ -172,7 +176,7 @@ class Crout {
                                         : ut.vals()[c_idx] -= ld * *U_v_itr;
         }
         // advance augmented handle
-        aug_id = L.nest_row_id(aug_id);
+        aug_id = L.next_row_id(aug_id);
       }  // while
     }
   }
@@ -205,8 +209,6 @@ class Crout {
     static_assert(U_augCrsType::ROW_MAJOR, "U must be AugCRS");
     using index_type = typename U_augCrsType::index_type;
 
-    psmilu_assert(IsSymm && m, "fatal, symmetric block cannot be empty!");
-
     if (!_step) return;
     // get the aug handle
     const size_type start  = IsSymm ? m : _step;
@@ -223,21 +225,51 @@ class Crout {
     }
   }
 
-  template <bool IsSymm, class DiagType, class PermType, class SpVecType>
+  template <bool IsSymm, class SpVecType, class DiagType>
   inline typename std::enable_if<!IsSymm>::type update_B_diag(
-      const DiagType & /* d */, const PermType & /* p */,
-      const SpVecType & /* l */, const SpVecType & /* ut */,
-      const size_type /* m */) const {
-    // do nothing
+      const SpVecType &l, const SpVecType &ut, const size_type m,
+      DiagType &d) const {
+    // get the current diagonal entry
+    const auto dk = d[_step];
+    // NOTE since l and ut are in general unsorted, we need to loop through
+    // one of them and determine the existance of the other entry, this can
+    // be done by utilizing the dense tags in sparse vector
+    const auto &    l_d_tags = l.dense_tags();
+    const size_type n        = ut.size();
+    // NOTE we choose to loop out ut, cuz in general length(ut) <= length(l)
+    // otherwise, another checking is needed for index bound
+    for (size_type i = 0u; i < n; ++i) {
+      const auto c_idx = ut.c_idx(i);
+      psmilu_assert(
+          (size_type)c_idx > _step,
+          "should only contain the upper part of ut, (c_idx,step)=(%zd,%zd)!",
+          (size_type)c_idx, _step);
+      // if the dense tags of this entry points to this step, we know l has
+      // an element in this slot
+      if (c_idx < m && (size_type)l_d_tags[c_idx] == _step)
+        d[c_idx] -= dk * ut.val(i) * l.vals()[c_idx];
+    }
   }
 
-  template <bool IsSymm, class DiagType, class PermType, class SpVecType>
+  template <bool IsSymm, class SpVecType, class DiagType>
   inline typename std::enable_if<IsSymm>::type update_B_diag(
-      const DiagType &d, const PermType &p, const SpVecType &l,
-      const SpVecType &ut, const size_type m) const {
+      const SpVecType &l, const SpVecType & /* ut */, const size_type m,
+      DiagType &d) const {
     psmilu_assert(m, "fatal, symmetric block cannot be empty!");
-
-    const auto d_pk = d[p[_step]];
+    // get the current diagonal entry
+    const auto dk = d[_step];
+    // NOTE that the diagonal is updated before doing any dropping/sorting,
+    // thus the indices are, in general, unsorted
+    const size_type n = l.size();
+    // get the c index
+    for (size_type i = 0u; i < n; ++i) {
+      const auto c_idx = l.c_idx(i);
+      psmilu_assert(
+          (size_type)c_idx > _step,
+          "should only contain the lower part of l, (c_idx,step)=(%zd,%zd)!",
+          (size_type)c_idx, _step);
+      if (c_idx < m) d[c_idx] -= dk * l.val(i) * l.val(i);
+    }
   }
 
  protected:
@@ -246,9 +278,10 @@ class Crout {
   /// \tparam PermType permutation vector type, see \ref BiPermMatrix
   /// \tparam SpVecType sparse vector type, see \ref SparseVector
   /// \param[in] ccs_A input matrix in CCS scheme
-  /// \param[in] p permutation vector with inverse enabled
+  /// \param[in] p left-hand side row permutation matrix
   /// \param[in] qk column index (C-based) in permutated matrix
-  /// \param[out] l output sparse vector
+  /// \param[out] l output sparse vector of column vector for L
+  /// \note Complexity is \f$\mathcal{O}(nnz(\boldsymbol{A}(:,qk)))\f$
   template <class CcsType, class PermType, class SpVecType>
   inline void _load_A2l(const CcsType &ccs_A, const PermType &p,
                         const size_type qk, SpVecType &l) const {
@@ -264,15 +297,13 @@ class Crout {
     for (auto last = ccs_A.row_ind_cend(qk); i_itr != last; ++i_itr, ++v_itr) {
       const auto c_idx = p.inv(to_c_idx<size_type, base>(*i_itr));
       // push to the sparse vector only if its in range _step+1:n
-      if (c_idx > _step) {
+      if ((size_type)c_idx > _step) {
+#ifndef NDEBUG
         const bool val_must_not_exit =
+#endif
             l.push_back(to_ori_idx<size_type, base>(c_idx), _step);
         psmilu_assert(val_must_not_exit,
                       "see prefix, failed on Crout step %zd for l", _step);
-#ifdef NDEBUG
-        // null statement for opt and getting rid of unused warning
-        (void)val_must_not_exit;
-#endif  // NDEBUG
         l.vals()[c_idx] = *v_itr;
       }
     }
@@ -285,35 +316,45 @@ class Crout {
   /// \tparam RightDiagType diagonal matrix from right-hand side
   /// \tparam PermType permutation vector type, see \ref BiPermMatrix
   /// \tparam SpVecType sparse vector type, see \ref SparseVector
+  /// \param[in] s diagonal matrix scaling from left-hand side
+  /// \param[in] crs_A input matrix in CRS scheme
+  /// \param[in] t diagonal matrix scaling from right-hand side
+  /// \param[in] q right-hand side column permutation matrix
+  /// \param[in] pk row index
+  /// \param[in] m leading size
+  /// \param[out] ut output sparse vector of row vector for U
+  /// \note Complexity is \f$\mathcol{O}(nnz(\boldsymbol{A}(pk,:)))\f$
   template <bool IsSymm, class LeftDiagType, class CrsType, class RightDiagType,
             class PermType, class SpVecType>
   inline void _load_A2ut(const LeftDiagType &s, const CrsType &crs_A,
                          const RightDiagType &t, const PermType &q,
-                         const size_type pk, const size_type m, SpVecType &ut) {
+                         const size_type pk, const size_type m,
+                         SpVecType &ut) const {
     // compilation consistency checking
     static_assert(!(CrsType::ONE_BASED ^ SpVecType::ONE_BASED),
                   "inconsistent one-based in ccs and sparse vector");
     constexpr static bool base = CrsType::ONE_BASED;
     // ut should be empty
     psmilu_assert(ut.empty(), "ut should be empty while loading A");
-    psmilu_assert(IsSymm && m, "fatal, symmetric block cannot be empty!");
     // note m == 0 should be checked b4 calling this routine
     const size_type thres = IsSymm ? m - 1u : _step;
+    std::cout << "step=" << _step << ':' << "thres=" << thres << std::endl;
     // pk is c index
     auto       v_itr = crs_A.val_cbegin(pk);
     auto       i_itr = crs_A.col_ind_cbegin(pk);
     const auto s_pk  = s[pk];
     for (auto last = crs_A.col_ind_cend(pk); i_itr != last; ++i_itr, ++v_itr) {
       const auto c_idx = q.inv(to_c_idx<size_type, base>(*i_itr));
-      if (c_idx > thres) {
+      if ((size_type)c_idx > thres) {
+#ifndef NDEBUG
         const bool val_must_not_exit =
+#endif
             ut.push_back(to_ori_idx<size_type, base>(c_idx), _step);
         psmilu_assert(val_must_not_exit,
                       "see prefix, failed on Crout step %zd for ut", _step);
-#ifdef NDEBUG
         // null statement for opt and getting rid of unused warning
         (void)val_must_not_exit;
-#endif                                                // NDEBUG
+        std::cout << c_idx << std::endl;
         ut.vals()[c_idx] = s_pk * *v_itr * t[c_idx];  // scale here
       }
     }
