9851c4438c2df3a2a3e7d99f405327b831844175
Qiao Chen <qiao.chen@stonybrook.edu>
2019-01-30 15:10:20 -0500

    added prec interface

---
+++ src/psmilu_Prec.hpp
@@ -0,0 +1,164 @@
+//@HEADER
+//----------------------------------------------------------------------------
+//                Copyright (C) 2019 The PSMILU AUTHORS
+//----------------------------------------------------------------------------
+//@HEADER
+
+/// \file psmilu_Prec.hpp
+/// \brief Multilevel preconditioner interface
+/// \authors Qiao,
+
+#ifndef _PSMILU_PREC_HPP
+#define _PSMILU_PREC_HPP
+
+#include <list>
+#include <utility>
+
+#include "psmilu_CompressedStorage.hpp"
+#include "psmilu_PermMatrix.hpp"
+
+namespace psmilu {
+
+/// \class Prec
+/// \brief A single level preconditioner
+/// \tparam ValueType value data type, e.g. \a double
+/// \tparam IndexType index data type, e.g. \a int
+/// \tparam OneBased if \a false (default), using C-based index system
+/// \ingroup itr
+template <class ValueType, class IndexType, bool OneBased = false>
+struct Prec {
+  typedef ValueType                             value_type;  ///< value type
+  typedef IndexType                             index_type;  ///< index type
+  typedef CRS<value_type, index_type, OneBased> crs_type;    ///< crs type
+  typedef CCS<value_type, index_type, OneBased> ccs_type;    ///< ccs type
+  typedef BiPermMatrix<index_type>              perm_type;   ///< permutation
+  typedef typename crs_type::size_type          size_type;   ///< size
+  typedef typename crs_type::array_type         array_type;  ///< array
+  static constexpr bool ONE_BASED  = OneBased;               ///< c index flag
+  static constexpr char EMPTY_PREC = '\0';                   ///< empty prec
+
+  /// \brief default constructor
+  Prec() = default;
+
+  /// \brief empty copy constructor
+  Prec(const Prec &) {}
+
+  /// \brief rvalue reference constructor
+  /// \param[in] mm leading size
+  /// \param[in] nn system size
+  /// \param[in] L_b lower part
+  /// \param[in] d_b diagonal
+  /// \param[in] U_b upper part
+  /// \param[in] e E part
+  /// \param[in] f F part
+  /// \param[in] S row scaling
+  /// \param[in] T column scaling
+  /// \param[in] P row permutation
+  /// \param[in] Q column permutation
+  /// \note This allows us to use emplace back in STL efficiently
+  Prec(index_type mm, index_type nn, ccs_type &&L_b, array_type &&d_b,
+       crs_type &&U_b, crs_type &&e, crs_type &&f, array_type &&S,
+       array_type &&T, perm_type &&P, perm_type &&Q)
+      : m(mm),
+        n(nn),
+        L_B(std::move(L_b)),
+        d_B(std::move(d_b)),
+        U_B(std::move(U_b)),
+        E(std::move(e)),
+        F(std::move(f)),
+        s(std::move(S)),
+        t(std::move(T)),
+        p(std::move(P)),
+        q(std::move(Q)) {}
+
+  /// \brief enable explicitly calling move
+  /// \param[in,out] L_b lower part
+  /// \param[in,out] d_b diagonal
+  /// \param[in,out] U_b upper part
+  /// \param[in,out] e E part
+  /// \param[in,out] f F part
+  /// \param[in,out] S row scaling
+  /// \param[in,out] T column scaling
+  /// \param[in,out] P row permutation
+  /// \param[in,out] Q column permutation
+  /// \note Sizes are not included, assign them explicitly.
+  ///
+  /// we pass lvalue reference, but will explicitly destroy all input arguments
+  /// on output. This allows one to avoid writing a ton of \a std::move while
+  /// calling this routine.
+  ///
+  /// \warn Everything on output is destroyed, as the routine name says.
+  inline void move_destroy(ccs_type &&L_b, array_type &&d_b, crs_type &&U_b,
+                           crs_type &&e, crs_type &&f, array_type &&S,
+                           array_type &&T, perm_type &&P, perm_type &&Q) {
+    L_B = std::move(L_b);
+    d_B = std::move(d_b);
+    U_B = std::move(U_b);
+    E   = std::move(e);
+    F   = std::move(f);
+    s   = std::move(S);
+    t   = std::move(T);
+    p   = std::move(P);
+    q   = std::move(Q);
+  }
+
+  index_type m;       ///< leading block size
+  index_type n;       ///< system size
+  ccs_type   L_B;     ///< lower part of leading block
+  array_type d_B;     ///< diagonal block of leading block
+  crs_type   U_B;     ///< upper part of leading block
+  crs_type   E;       ///< scaled and permutated E part
+  crs_type   F;       ///< scaled and permutated F part
+  array_type s;       ///< row scaling vector
+  array_type t;       ///< column scaling vector
+  perm_type  p;       ///< row permutation matrix
+  perm_type  q;       ///< column permutation matrix
+  void *     SC_plu;  ///< dense decomposition, TBD
+
+ private:
+  /// \brief allow casting to \a char, this is needed to add an empty node
+  Prec(char) {}
+
+  friend class std::list<Prec>;  // give list friendship
+};
+
+/// \typedef Precs
+/// \brief multilevel preconditioners
+/// \tparam ValueType value data type, e.g. \a double
+/// \tparam IndexType index data type, e.g. \a int
+/// \tparam OneBased if \a false (default), using C-based index system
+/// \ingroup itr
+///
+/// We choose to use STL list because adding new node is constant time without
+/// bothering copying. It's worth noting that querying the size of \a list has
+/// become constant since C++11. Also, with the constructors in \ref Prec, the
+/// following two ways can be used to construct list:
+///
+/// \code{.cpp}
+/// using precs_t = Precs<double, int>;
+/// using prec_t = precs_t::value_type;
+/// precs_t precs;
+/// precs.emplace_back(m, n, /* rvalue references */);
+/// \endcode
+///
+/// and
+///
+/// \code{.cpp}
+/// using precs_t = Precs<double, int>;
+/// using prec_t = precs_t::value_type;
+/// precs_t precs;
+/// precs.push_back(prec_t::EMPTY_PREC);
+/// auto &prec = precs.back();
+/// prec.move_destroy(/* lvalue references */);
+/// prec.m = m;
+/// prec.n = n;
+/// \endcode
+///
+/// These fit into the usage for Precs. Notice that for the second way, after
+/// calling \ref Prec::move_destroy, all input arguments will be destroyed.
+template <class ValueType, class IndexType, bool OneBased = false>
+using Precs = std::list<Prec<ValueType, IndexType, OneBased>>;
+
+}  // namespace psmilu
+
+#endif  // _PSMILU_PREC_HPP
