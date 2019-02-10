d61514d51ede480c5f519e9d95c46dd37ba26989
Qiao Chen <qiao.chen@stonybrook.edu>
2019-02-10 16:55:33 -0500

    completed utilities that are needed in ifac

--- src/psmilu_fac.hpp
+++ src/psmilu_fac.hpp
@@ -43,10 +43,10 @@ inline Array<typename CcsType::value_type> extract_perm_diag(
   auto v_begin = A.vals().cbegin();
   auto i_begin = A.row_ind().cbegin();
   for (size_type i = 0u; i < m; ++i) {
-    psmilu_assert(q[i] < A.ncols(),
+    psmilu_assert((size_type)q[i] < A.ncols(),
                   "permutated index %zd exceeds the col bound",
                   (size_type)q[i]);
-    psmilu_assert(p[i] < A.nrows(),
+    psmilu_assert((size_type)p[i] < A.nrows(),
                   "permutated index %zd exceeds the row bound",
                   (size_type)p[i]);
     auto info = find_sorted(A.row_ind_cbegin(q[i]), A.row_ind_cend(q[i]),
@@ -98,7 +98,7 @@ inline typename AugCcsType::ccs_type extract_L_B(
   if (!(col_start[m] - ONE_BASED)) {
     psmilu_warning(
         "exactly zero L_B, this most likely is a bug! Continue anyway...");
-    return;
+    return L_B;
   }
 
   L_B.reserve(col_start[m]);
@@ -107,8 +107,8 @@ inline typename AugCcsType::ccs_type extract_L_B(
   psmilu_error_if(row_ind.status() == DATA_UNDEF || vals.status() == DATA_UNDEF,
                   "memory allocation failed");
 
-  row_ind.resize(col_start[m]);
-  vals.resize(col_start[m]);
+  row_ind.resize(col_start[m] - ONE_BASED);
+  vals.resize(col_start[m] - ONE_BASED);
 
   auto itr   = row_ind.begin();
   auto v_itr = vals.begin();
@@ -161,10 +161,10 @@ inline typename AugCrsType::ccs_type extract_U_B(
   // compute nnz
   for (size_type i = 0u; i < m; ++i) col_start[i + 1] += col_start[i];
 
-  if (!(col_start[m] - ONE_BASED)) {
+  if (!col_start[m]) {
     psmilu_warning(
         "exactly zero L_B, this most likely is a bug! Continue anyway...");
-    return;
+    return U_B;
   }
 
   U_B.reserve(col_start[m]);
@@ -178,7 +178,7 @@ inline typename AugCrsType::ccs_type extract_U_B(
     auto itr  = U.col_ind_cbegin(i);
     for (auto v_itr = U.val_cbegin(i); itr != last; ++itr, ++v_itr) {
       const auto j          = c_idx(*itr);
-      row_ind[col_start[j]] = *itr;
+      row_ind[col_start[j]] = i;
       vals[col_start[j]]    = *v_itr;
       ++col_start[j];
     }
@@ -198,11 +198,225 @@ inline typename AugCrsType::ccs_type extract_U_B(
 }
 
 /// \ingroup fac
-template <class CcsType, class PermType>
-inline CcsType extract_E(const CcsType &A, const typename CcsType::size_type m,
-                         const PermType &p, const PermType &q) {
-  static_assert(!CcsType::ROW_MAJOR, "input A must be CCS!");
+template <class CrsType, class PermType>
+inline typename CrsType::other_type extract_E(
+    const CrsType &A, const typename CrsType::size_type m, const PermType &p,
+    const PermType &q) {
+  // it's efficient to extract E from CRS
+  static_assert(CrsType::ROW_MAJOR, "input A must be CRS!");
+  using ccs_type   = typename CrsType::other_type;
+  using size_type  = typename CrsType::size_type;
+  using index_type = typename CrsType::index_type;
+  const auto c_idx = [](const size_type i) {
+    return to_c_idx<size_type, CrsType::ONE_BASED>(i);
+  };
+
+  const size_type n = A.nrows();
+
+  psmilu_error_if(m > n || m > A.ncols(),
+                  "leading block size %zd should not exceed matrix size", m);
+  const size_type N = n - m;
+  ccs_type        E(N, m);
+  if (!N) {
+    psmilu_warning("empty E matrix detected!");
+    return E;
+  }
+
+  auto &col_start = E.col_start();
+  col_start.resize(m + 1);
+  psmilu_error_if(col_start.status() == DATA_UNDEF, "memory allocation failed");
+  std::fill(col_start.begin(), col_start.end(), 0);
+
+  for (size_type i = m; i < n; ++i) {
+    for (auto itr = A.col_ind_cbegin(p[i]), last = A.cool_ind_cend(p[i]);
+         itr != last; ++itr) {
+      const size_type qi = q.inv(c_idx(*itr));
+      if (qi < m) ++col_start[qi + 1];
+    }
+  }
+
+  // accumulate for nnz
+  for (size_type i = 0u; i < m; ++i) col_start[i + 1] += col_start[i];
+
+  if (!col_start[m]) {
+    psmilu_warning(
+        "exactly zero E, this most likely is a bug! Continue anyway...");
+    return E;
+  }
+
+  E.reserve(col_start[m]);
+  auto &row_ind = E.row_ind();
+  auto &vals    = E.vals();
+
+  psmilu_error_if(row_ind.status() == DATA_UNDEF || vals.status() == DATA_UNDEF,
+                  "memory allocation failed");
+
+  row_ind.resize(col_start[m]);
+  vals.resize(col_start[m]);
+
+  for (size_type i = m; i < n; ++i) {
+    const size_type pi  = p[i];
+    auto            itr = A.col_ind_cbegin(pi), last = A.col_ind_cend(pi);
+    auto            v_itr = A.val_cbegin(pi);
+    for (; itr != last; ++itr, ++v_itr) {
+      const size_type qi = q.inv(c_idx(*itr));
+      if (qi < m) {
+        row_ind[col_start[qi]] = i - m;
+        vals[col_start[qi]]    = *v_itr;
+        ++col_start[qi];
+      }
+    }
+  }
+
+  // revert
+  index_type tmp(0);
+  for (size_type i = 0u; i < m; ++i) std::swap(col_start[i], tmp);
+
+  if (CrsType::ONE_BASED)
+    std::for_each(col_start.begin(), col_start.end(),
+                  [](index_type &i) { ++i; });
+
+  return E;
 }
+
+/// \ingroup fac
+template <class CcsType, class PermType, class BufferType>
+inline CcsType extract_F(const CcsType &A, const typename CcsType::size_type m,
+                         const PermType &p, const PermType &q,
+                         BufferType &buf) {
+  static_assert(!CcsType::ROW_MAJOR, "input A must be CCS!");
+  using size_type                 = typename CcsType::size_type;
+  using index_type                = typename CcsType::index_type;
+  constexpr static bool ONE_BASED = CcsType::ONE_BASED;
+  const auto            c_idx     = [](const size_type i) {
+    return to_c_idx<size_type, ONE_BASED>(i);
+  };
+
+  const size_type n = A.ncols();
+
+  psmilu_error_if(m > n || m > A.nrows(),
+                  "leading block size %zd should not exceed matrix size", m);
+
+  const size_type N = n - m;
+  CcsType         F(m, N);
+  if (!N) {
+    psmilu_warning("empty F matrix detected!");
+    return F;
+  }
+
+  auto &col_start = F.col_start();
+  col_start.resize(N + 1);
+  psmilu_error_if(col_start.status() == DATA_UNDEF, "memory allocation failed");
+  col_start.front() = ONE_BASED;
+
+  for (size_type i = 0u; i < N; ++i) {
+    const size_type pi = p[i + m];
+    size_type       nnz(0);
+    std::for_each(A.row_ind_cbegin(pi), A.row_ind_cend(pi),
+                  [&](const index_type i) {
+                    if (q.inv(c_idx(i)) < m) ++nnz;
+                  });
+    col_start[i + 1] = col_start[i] + nnz;
+  }
+
+  if (!(col_start[m] - ONE_BASED)) {
+    psmilu_warning(
+        "exactly zero F, this most likely is a bug! Continue anyway...");
+    return F;
+  }
+
+  F.reserve(col_start[m]);
+  auto &row_ind = F.row_ind();
+  auto &vals    = F.vals();
+  psmilu_error_if(row_ind.status() == DATA_UNDEF || vals.status() == DATA_UNDEF,
+                  "memory allocation failed");
+
+  row_ind.resize(col_start[m] - ONE_BASED);
+  vals.resize(col_start[m] - ONE_BASED);
+
+  auto v_itr = vals.begin();
+
+  for (size_type i = 0u; i < N; ++i) {
+    const size_type pi      = p[i + m];
+    auto            itr     = F.row_ind_begin(i);
+    auto            A_itr   = A.row_ind_cbegin(pi);
+    auto            A_v_itr = A.val_cbegin(pi);
+    for (auto A_last = A.row_ind_cend(pi); A_itr != A_last;
+         ++A_itr, ++A_v_itr) {
+      const size_type qi = q.inv(c_idx(*A_itr));
+      if (qi < m) {
+        *itr = qi;
+        ++itr;
+        buf[qi] = *A_v_itr;
+      }
+    }
+    std::sort(F.row_ind_begin(i), itr);
+    if (!ONE_BASED)
+      for (auto itr = F.row_ind_cbegin(i), last = F.row_ind_cend(i);
+           itr != last; ++itr, ++v_itr)
+        *v_itr = buf[*itr];
+    else {
+      auto last = F.row_ind_end(i);
+      for (itr = F.row_ind_begin(i); itr != last; ++itr, ++v_itr) {
+        *v_itr = buf[*itr];
+        ++*itr;
+      }
+    }
+  }
+  return F;
+}
+
+/// \class CompressedTypeTrait
+/// \ingroup fac
+template <class CsType1, class CsType2>
+class CompressedTypeTrait {
+  static_assert(CsType1::ROW_MAJOR ^ CsType2::ROW_MAJOR,
+                "cannot have same type");
+  constexpr static bool _1_IS_CRS = CsType1::ROW_MAJOR;
+
+ public:
+  typedef typename std::conditional<_1_IS_CRS, CsType1, CsType2>::type crs_type;
+  ///< \ref CRS type
+  typedef typename crs_type::other_type ccs_type;  ///< \ref CCS type
+
+  /// \brief choose crs type from two inputs
+  /// \param[in] A crs type
+  /// \return reference to \a A
+  /// \sa crs_type, select_ccs
+  template <class T = void>
+  inline static const crs_type &select_crs(
+      const CsType1 &A, const CsType2 & /* B */,
+      const typename std::enable_if<_1_IS_CRS, T>::type * = nullptr) {
+    return A;
+  }
+
+  // dual version
+  template <class T = void>
+  inline static const crs_type &select_crs(
+      const CsType1 & /* A */, const CsType2 &B,
+      const typename std::enable_if<!_1_IS_CRS, T>::type * = nullptr) {
+    return B;
+  }
+
+  /// \brief choose ccs type from two inputs
+  /// \param[in] B ccs type
+  /// \return reference to \a B
+  /// \sa ccs_type, select_crs
+  template <class T = void>
+  inline static const ccs_type &select_ccs(
+      const CsType1 & /* A */, const CsType2 &B,
+      const typename std::enable_if<_1_IS_CRS, T>::type * = nullptr) {
+    return B;
+  }
+
+  // dual version
+  template <class T = void>
+  inline static const ccs_type &select_ccs(
+      const CsType1 &A, const CsType2 & /* B */,
+      const typename std::enable_if<!_1_IS_CRS, T>::type * = nullptr) {
+    return A;
+  }
+};
 
 }  // namespace internal
 }  // namespace psmilu
