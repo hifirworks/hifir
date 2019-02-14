814b9ed4acc551feb6629863edaa235bab1e5582
Qiao Chen <qiao.chen@stonybrook.edu>
2019-02-13 21:07:27 -0500

    added handling exception to make the impl more robust

--- bench/GMRES.hpp
+++ bench/GMRES.hpp
@@ -17,9 +17,9 @@
 #include <cstdio>
 #include <limits>
 #include <numeric>
+#include <stdexcept>
 #include <string>
 #include <tuple>
-#include <type_traits>
 #include <utility>
 #include <vector>
 
@@ -90,12 +90,32 @@ const static struct {
 }  // namespace internal
 
 enum {
+  GMRES_UNKNOWN_ERROR     = -3,
   GMRES_INVALID_FUNC_PARS = -2,
   GMRES_INVALID_PARS      = -1,
   GMRES_SUCCESS           = 0,
   GMRES_DIVERGED          = 1,
   GMRES_STAGNATED         = 2
 };
+
+inline const char *gmres_repr_flag(const int flag) {
+  switch (flag) {
+    case GMRES_UNKNOWN_ERROR:
+      return "GMRES_UNKNOWN_ERROR";
+    case GMRES_INVALID_FUNC_PARS:
+      return "GMRES_INVALID_FUNC_PARS";
+    case GMRES_INVALID_PARS:
+      return "GMRES_INVALID_PARS";
+    case GMRES_SUCCESS:
+      return "GMRES_SUCCESS";
+    case GMRES_DIVERGED:
+      return "GMRES_DIVERGED";
+    case GMRES_STAGNATED:
+      return "GMRES_STAGNATED";
+    default:
+      return "GMRES_INVALID_FLAG";
+  }
+}
 
 /// \class GMRES
 /// \tparam ValueType value type used, default is \a double
@@ -210,120 +230,130 @@ class GMRES {
     using internal::conj;
     const static scalar_type _stag_eps =
         std::pow(scalar_type(10), -(scalar_type)_D);
-
-    const size_type n = b.size();
-    size_type       iter(0);
-    if (n != x0.size())
-      return std::make_tuple(GMRES_INVALID_FUNC_PARS, iter, 0.0);
-    if (rtol <= 0 || restart <= 0 || maxit <= 0)
-      return std::make_tuple(GMRES_INVALID_PARS, iter, 0.0);
-    const auto beta0 = std::sqrt(
-        std::inner_product(b.cbegin(), b.cend(), b.cbegin(), value_type()));
-    if (_M.empty() || recompute_M) _M.compute(A, M_pars);
-    // record  time after preconditioner
-    auto time_start = std::chrono::high_resolution_clock::now();
-    _ensure_data_capacities(n);
-    const size_type max_outer_iters =
-        (size_type)std::ceil((scalar_type)maxit / restart);
-    auto &      x    = x0;
-    int         flag = GMRES_SUCCESS;
-    scalar_type resid(1);
-    for (size_type it_outer = 0u; it_outer < max_outer_iters; ++it_outer) {
-      Cout("Enter outer iteration %zd.\n", it_outer + 1);
-      if (it_outer)
-        Cerr("\033[1;33mWARNING!\033[0m Couldn\'t solve with %d restarts.\n",
-             restart);
-      // initial residual
-      if (it_outer > 0u || std::inner_product(x.cbegin(), x.cend(), x.cbegin(),
-                                              value_type()) > 0) {
-        A.mv(x, _v);
-        for (size_type i = 0u; i < n; ++i) _v[i] = b[i] - _v[i];
-      } else
-        std::copy(b.cbegin(), b.cend(), _v.begin());
-      const auto beta = std::sqrt(std::inner_product(
-          _v.cbegin(), _v.cend(), _v.cbegin(), value_type()));
-      _y[0]           = beta;
-      {
-        const auto inv_beta = 1. / beta;
-        for (size_type i = 0u; i < n; ++i) _Q[i] = _v[i] * inv_beta;
-      }
-      size_type j(0);
-      for (;;) {
-        const auto jn = j * n;
-        std::copy(_Q.cbegin() + jn, _Q.cbegin() + jn + n, _w.begin());
-        _M.solve(_w, _v);
-        std::copy(_v.cbegin(), _v.cend(), _Z.begin() + jn);
-        A.mv(_v, _w);
-        for (size_type k = 0u; k <= j; ++k) {
-          auto itr = _Q.cbegin() + k * n;
-          _v[k] = std::inner_product(_w.cbegin(), _w.cend(), itr, value_type());
-          const auto tmp = _v[k];
-          for (size_type i = 0u; i < n; ++i) _w[i] -= tmp * itr[i];
+    size_type iter(0);
+    try {
+      const size_type n = b.size();
+      if (n != x0.size())
+        return std::make_tuple(GMRES_INVALID_FUNC_PARS, iter, 0.0);
+      if (rtol <= 0 || restart <= 0 || maxit <= 0)
+        return std::make_tuple(GMRES_INVALID_PARS, iter, 0.0);
+      const auto beta0 = std::sqrt(
+          std::inner_product(b.cbegin(), b.cend(), b.cbegin(), value_type()));
+      if (_M.empty() || recompute_M) _M.compute(A, M_pars);
+      // record  time after preconditioner
+      auto time_start = std::chrono::high_resolution_clock::now();
+      _ensure_data_capacities(n);
+      const size_type max_outer_iters =
+          (size_type)std::ceil((scalar_type)maxit / restart);
+      auto &      x    = x0;
+      int         flag = GMRES_SUCCESS;
+      scalar_type resid(1);
+      for (size_type it_outer = 0u; it_outer < max_outer_iters; ++it_outer) {
+        Cout("Enter outer iteration %zd.\n", it_outer + 1);
+        if (it_outer)
+          Cerr("\033[1;33mWARNING!\033[0m Couldn\'t solve with %d restarts.\n",
+               restart);
+        // initial residual
+        if (it_outer > 0u || std::inner_product(x.cbegin(), x.cend(),
+                                                x.cbegin(), value_type()) > 0) {
+          A.mv(x, _v);
+          for (size_type i = 0u; i < n; ++i) _v[i] = b[i] - _v[i];
+        } else
+          std::copy(b.cbegin(), b.cend(), _v.begin());
+        const auto beta = std::sqrt(std::inner_product(
+            _v.cbegin(), _v.cend(), _v.cbegin(), value_type()));
+        _y[0]           = beta;
+        {
+          const auto inv_beta = 1. / beta;
+          for (size_type i = 0u; i < n; ++i) _Q[i] = _v[i] * inv_beta;
         }
-        const auto w_norm2 = std::inner_product(_w.cbegin(), _w.cend(),
-                                                _w.cbegin(), value_type());
-        const auto w_norm  = std::sqrt(w_norm2);
-        if (j + 1 < (size_type)restart) {
-          auto       itr      = _Q.begin() + jn + n;
-          const auto inv_norm = 1. / w_norm;
-          for (size_type i = 0u; i < n; ++i) itr[i] = inv_norm * _w[i];
-        }
-        auto J1 = _J.begin(), J2 = J1 + restart;
-        for (size_type colJ = 0u; colJ + 1u <= j; ++colJ) {
-          const auto tmp = _v[colJ];
-          _v[colJ]       = conj(J1[colJ]) * tmp + conj(J2[colJ]) * _v[colJ + 1];
-          _v[colJ + 1]   = -J2[colJ] * tmp + J1[colJ] * _v[colJ + 1];
-        }
-        const auto rho = std::sqrt(_v[j] * _v[j] + w_norm2);
-        J1[j]          = _v[j] / rho;
-        J2[j]          = w_norm / rho;
-        _y[j + 1]      = -J2[j] * _y[j];
-        _y[j]          = conj(J1[j]) * _y[j];
-        _v[j]          = rho;
-        std::copy(_v.cbegin(), _v.cbegin() + j + 1, _R.begin() + j * restart);
-        const auto resid_prev = resid;
-        resid                 = std::abs(_y[j + 1]) / beta0;
-        if (resid >= resid_prev * (1 - _stag_eps)) {
-          flag = GMRES_STAGNATED;
-          Cerr(
-              "\033[1;33mWARNING!\033[0m Stagnated detected at iteration "
-              "%zd.\n",
-              iter);
-          break;
-        } else if (iter >= maxit) {
-          Cerr("\033[1;33mWARNING!\033[0m Reached maxit iteration limit %zd.\n",
-               maxit);
-          flag = GMRES_DIVERGED;
-          break;
-        }
-        ++iter;
-        Cout("  At iteration %zd, relative residual is %g.\n", iter, resid);
-        _resids.push_back(resid);
-        if (resid < rtol || j >= (size_type)restart) break;
-        ++j;
-      }  // inf loop
-      // backsolve
-      if (j) {
-        auto R_itr = _R.cbegin() + j * restart;
-        for (int k = j; k > -1; --k) {
-          if (_y[k] != value_type()) {
-            _y[k] /= R_itr[k];
-            const auto tmp = _y[k];
-            for (int i = k - 1; i > -1; --i) _y[i] -= tmp * R_itr[i];
+        size_type j(0);
+        for (;;) {
+          const auto jn = j * n;
+          std::copy(_Q.cbegin() + jn, _Q.cbegin() + jn + n, _w.begin());
+          _M.solve(_w, _v);
+          std::copy(_v.cbegin(), _v.cend(), _Z.begin() + jn);
+          A.mv(_v, _w);
+          for (size_type k = 0u; k <= j; ++k) {
+            auto itr = _Q.cbegin() + k * n;
+            _v[k] =
+                std::inner_product(_w.cbegin(), _w.cend(), itr, value_type());
+            const auto tmp = _v[k];
+            for (size_type i = 0u; i < n; ++i) _w[i] -= tmp * itr[i];
           }
-          R_itr -= restart;
+          const auto w_norm2 = std::inner_product(_w.cbegin(), _w.cend(),
+                                                  _w.cbegin(), value_type());
+          const auto w_norm  = std::sqrt(w_norm2);
+          if (j + 1 < (size_type)restart) {
+            auto       itr      = _Q.begin() + jn + n;
+            const auto inv_norm = 1. / w_norm;
+            for (size_type i = 0u; i < n; ++i) itr[i] = inv_norm * _w[i];
+          }
+          auto J1 = _J.begin(), J2 = J1 + restart;
+          for (size_type colJ = 0u; colJ + 1u <= j; ++colJ) {
+            const auto tmp = _v[colJ];
+            _v[colJ]     = conj(J1[colJ]) * tmp + conj(J2[colJ]) * _v[colJ + 1];
+            _v[colJ + 1] = -J2[colJ] * tmp + J1[colJ] * _v[colJ + 1];
+          }
+          const auto rho = std::sqrt(_v[j] * _v[j] + w_norm2);
+          J1[j]          = _v[j] / rho;
+          J2[j]          = w_norm / rho;
+          _y[j + 1]      = -J2[j] * _y[j];
+          _y[j]          = conj(J1[j]) * _y[j];
+          _v[j]          = rho;
+          std::copy(_v.cbegin(), _v.cbegin() + j + 1, _R.begin() + j * restart);
+          const auto resid_prev = resid;
+          resid                 = std::abs(_y[j + 1]) / beta0;
+          if (resid >= resid_prev * (1 - _stag_eps)) {
+            flag = GMRES_STAGNATED;
+            Cerr(
+                "\033[1;33mWARNING!\033[0m Stagnated detected at iteration "
+                "%zd.\n",
+                iter);
+            break;
+          } else if (iter >= maxit) {
+            Cerr(
+                "\033[1;33mWARNING!\033[0m Reached maxit iteration limit "
+                "%zd.\n",
+                maxit);
+            flag = GMRES_DIVERGED;
+            break;
+          }
+          ++iter;
+          Cout("  At iteration %zd, relative residual is %g.\n", iter, resid);
+          _resids.push_back(resid);
+          if (resid < rtol || j >= (size_type)restart) break;
+          ++j;
+        }  // inf loop
+        // backsolve
+        if (j) {
+          auto R_itr = _R.cbegin() + j * restart;
+          for (int k = j; k > -1; --k) {
+            if (_y[k] != value_type()) {
+              _y[k] /= R_itr[k];
+              const auto tmp = _y[k];
+              for (int i = k - 1; i > -1; --i) _y[i] -= tmp * R_itr[i];
+            }
+            R_itr -= restart;
+          }
         }
+        for (size_type i = 0u; i <= j; ++i) {
+          const auto tmp   = _y[i];
+          auto       Z_itr = _Z.cbegin() + i * n;
+          for (size_type k = 0u; k < n; ++k) x[k] += tmp * Z_itr[k];
+        }
+        if (resid < rtol || flag != GMRES_SUCCESS) break;
       }
-      for (size_type i = 0u; i <= j; ++i) {
-        const auto tmp   = _y[i];
-        auto       Z_itr = _Z.cbegin() + i * n;
-        for (size_type k = 0u; k < n; ++k) x[k] += tmp * Z_itr[k];
-      }
-      if (resid < rtol || flag != GMRES_SUCCESS) break;
+      auto time_end = std::chrono::high_resolution_clock::now();
+      std::chrono::duration<double> period = time_end - time_start;
+      return std::make_tuple(flag, iter, period.count());
+    } catch (const std::exception &e) {
+      Cerr(
+          "\033[1;33mWARNING!\033[0m Unexpected termination due to exception "
+          "threw at iteration %zd.\n\n\tThe message is:\n\n%s\n",
+          iter, e.what());
+      return std::make_tuple(GMRES_UNKNOWN_ERROR, iter, 0.0);
     }
-    auto time_end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> period = time_end - time_start;
-    return std::make_tuple(flag, iter, period.count());
   }
 };
 
