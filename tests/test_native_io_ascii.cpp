5c7ab38652c441010974d610e952285372df8b4a
Qiao Chen <qiao.chen@stonybrook.edu>
2019-02-20 19:30:26 -0500

    Added full test for ascii file IO

--- tests/test_native_io_ascii.cpp
+++ tests/test_native_io_ascii.cpp
@@ -15,10 +15,47 @@
 using namespace psmilu;
 
 TEST(IO, crs) {
-    const int n  = RandIntGen(100, 300)();
+  const int n = RandIntGen(100, 300)();
   {
     using crs_t  = CRS<double, int>;
     const auto A = gen_rand_sparse<crs_t>(n, n);
-    A.write_native_ascii("foo.psmilu");
+    A.write_native_ascii("foo1.psmilu");
+    crs_t B;
+    B.read_native_ascii("foo1.psmilu");
+    const auto Ad = convert2dense(A);
+    const auto Bd = convert2dense(B);
+    COMPARE_MATS(Ad, Bd);
+    CRS<double, int, true> C;
+    C.read_native_ascii("foo1.psmilu");
+    const auto Cd = convert2dense(C);
+    COMPARE_MATS(Ad, Cd);
+    crs_t::other_type D;
+    D.read_native_ascii("foo1.psmilu");
+    const auto Dd = convert2dense(D);
+    COMPARE_MATS(Ad, Dd);
+  }
+}
+
+TEST(IO, ccs) {
+  const int n = RandIntGen(100, 300)();
+  {
+    using ccs_t  = CCS<double, int>;
+    const auto A = gen_rand_sparse<ccs_t>(n, n);
+    A.write_native_ascii("foo2.psmilu");
+    ccs_t B;
+    B.read_native_ascii("foo2.psmilu");
+
+    const auto Ad = convert2dense(A);
+    const auto Bd = convert2dense(B);
+    COMPARE_MATS(Ad, Bd);
+    CCS<double, int, true> C;
+    C.read_native_ascii("foo2.psmilu");
+    const auto Cd = convert2dense(C);
+    COMPARE_MATS(Ad, Cd);
+    ccs_t::other_type D;
+
+    D.read_native_ascii("foo2.psmilu");
+    const auto Dd = convert2dense(D);
+    COMPARE_MATS(Ad, Dd);
   }
 }
