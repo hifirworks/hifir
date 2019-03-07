52ba9d098bd81f4bc23bcdec5ba701ffb324a907
Qiao Chen <qiao.chen@stonybrook.edu>
2019-03-07 18:55:47 -0500

    added some python skeletons

---
+++ python/psmilu4py/common.hpp
@@ -0,0 +1,15 @@
+//@HEADER
+//----------------------------------------------------------------------------
+//                Copyright (C) 2019 The PSMILU AUTHORS
+//----------------------------------------------------------------------------
+//@HEADER
+
+#ifndef _PSMILU_PYTHON_COMMON_HPP
+#define _PSMILU_PYTHON_COMMON_HPP
+
+// NOTE we need to ensure C++ code throws exceptions
+#ifndef PSMILU_THROW
+#  define PSMILU_THROW
+#endif  // PSMILU_THROW
+
+#endif  // _PSMILU_PYTHON_COMMON_HPP
