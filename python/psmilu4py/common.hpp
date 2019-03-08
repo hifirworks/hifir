63de82babf4a83c50a931228a9bbf73926e2a7d5
Qiao Chen <qiao.chen@stonybrook.edu>
2019-03-08 13:33:44 -0500

    added checking for file dp macros and remove them if neccessary

--- python/psmilu4py/common.hpp
+++ python/psmilu4py/common.hpp
@@ -4,6 +4,9 @@
 //----------------------------------------------------------------------------
 //@HEADER
 
+// Authors:
+//  Qiao,
+
 #ifndef _PSMILU_PYTHON_COMMON_HPP
 #define _PSMILU_PYTHON_COMMON_HPP
 
@@ -11,5 +14,13 @@
 #ifndef PSMILU_THROW
 #  define PSMILU_THROW
 #endif  // PSMILU_THROW
+
+// we need to make sure that the stdout and stderr are not pre-defined
+#ifdef PSMILU_STDOUT
+#  undef PSMILU_STDOUT
+#endif  // PSMILU_STDOUT
+#ifdef PSMILU_STDERR
+#  undef PSMILU_STDERR
+#endif  // PSMILU_STDOUT
 
 #endif  // _PSMILU_PYTHON_COMMON_HPP
