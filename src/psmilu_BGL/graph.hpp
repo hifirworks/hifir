918b71ea8e39b522a7e731175a837358b86c2813
Qiao Chen <qiao.chen@stonybrook.edu>
2019-06-12 22:49:54 -0400

    added BGL library support

---
+++ src/psmilu_BGL/graph.hpp
@@ -0,0 +1,93 @@
+//@HEADER
+//----------------------------------------------------------------------------
+//                Copyright (C) 2019 The PSMILU AUTHORS
+//----------------------------------------------------------------------------
+//@HEADER
+
+/// \file psmilu_BGL/graph.hpp
+/// \brief Interface for creating undirected graph
+/// \authors Qiao,
+
+#ifndef _PSMILU_BGL_GRAPH_HPP
+#define _PSMILU_BGL_GRAPH_HPP
+
+#include <utility>
+
+#define BOOST_NO_HASH
+
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/properties.hpp>
+
+#include "psmilu_Array.hpp"
+#include "psmilu_log.hpp"
+
+namespace psmilu {
+
+namespace internal {
+
+/// \brief create an undirected graph
+/// \tparam IndexType index type, e.g. \a int
+/// \ingroup pre
+///
+/// Notice that we use undirected graph, because for symmetric systems, we
+/// only store the strict lower part, whereas the whole matrix (except for
+/// diagonals) for general systems, which is equiv to A+A'.
+template <class IndexType>
+struct BGL_UndirectedGraphTrait {
+  using index_type      = IndexType;  ///< index type
+  using vertex_property = boost::property<boost::vertex_index_t, index_type>;
+  ///< vertex property
+  using edge_property = boost::property<boost::edge_index_t, index_type>;
+  ///< edge property
+  using graph_type =
+      boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS,
+                            vertex_property, edge_property, boost::no_property,
+                            boost::vecS>;
+  ///< graph type
+};
+
+}  // namespace internal
+
+/// \brief create graph
+/// \tparam IsSymm if \a true, then assume symmetric input
+/// \tparam CcsType_C input matrix storage \ref CCS in C index order
+/// \param[in] B input matrix
+/// \return graph representation of matrix \a B
+template <bool IsSymm, class CcsType_C>
+inline typename internal::BGL_UndirectedGraphTrait<
+    typename CcsType_C::index_type>::graph_type
+create_graph(const CcsType_C &B) {
+  using index_type = typename CcsType_C::index_type;
+  using graph_type =
+      typename internal::BGL_UndirectedGraphTrait<index_type>::graph_type;
+  using size_type = typename CcsType_C::size_type;
+  static_assert(!CcsType_C::ONE_BASED, "must be C index");
+  using edge_type = std::pair<index_type, index_type>;
+
+  const size_type nv = B.ncols();  // number of vertices
+  size_type       ne = B.nnz();    // upper bound for number of edges
+
+  Array<edge_type> edges(ne);
+  psmilu_error_if(edges.status() == DATA_UNDEF, "memory allocation failed");
+  size_type i(0);
+  for (size_type col(0); col < nv; ++col) {
+    if (IsSymm) {
+      if (B.nnz_in_col(col)) {
+        auto itr = B.row_ind_cbegin(col);
+        if (*itr == (index_type)col) ++itr;
+        for (auto j = itr; j != B.row_ind_cend(col); ++j)
+          edges[i++] = std::make_pair(static_cast<index_type>(col), *j);
+      }
+    } else {
+      for (auto itr = B.row_ind_cbegin(col); itr != B.row_ind_cend(col); ++itr)
+        if (*itr != (index_type)col)
+          edges[i++] = std::make_pair(static_cast<index_type>(col), *itr);
+    }
+  }
+  ne = i;
+  edges.resize(ne);
+  return graph_type(edges.cbegin(), edges.cend(), nv, ne);
+}
+}  // namespace psmilu
+
+#endif  // _PSMILU_BGL_GRAPH_HPP
---
+++ src/psmilu_BGL/rcm.hpp
@@ -0,0 +1,63 @@
+//@HEADER
+//----------------------------------------------------------------------------
+//                Copyright (C) 2019 The PSMILU AUTHORS
+//----------------------------------------------------------------------------
+//@HEADER
+
+/// \file psmilu_BGL/rcm.hpp
+/// \brief Interface for calling BGL's reverse Cuthill Mckee (RCM)
+/// \authors Qiao,
+
+#ifndef _PSMILU_BGL_RCM_HPP
+#define _PSMILU_BGL_RCM_HPP
+
+#include <vector>
+
+#include "graph.hpp"
+
+#include <boost/graph/cuthill_mckee_ordering.hpp>
+
+#include "psmilu_Options.h"
+
+namespace psmilu {
+/// \brief use RCM algorithm to reduce the bandwidth
+/// \tparam IsSymm if \a true, use symmetric version (undirected graph)
+/// \tparam CcsType_C c index \ref CCS matrix
+/// \param[in] B input matrix with only *sparsity pattern*
+/// \param[in] opt options
+/// \return permutation vector
+/// \ingroup pre
+/// \note This requires Boost Graph Library (BGL)
+template <bool IsSymm, class CcsType_C>
+inline Array<typename CcsType_C::index_type> run_rcm(const CcsType_C &B,
+                                                     const Options &  opt) {
+  using size_type = typename CcsType_C::size_type;
+  static_assert(!CcsType_C::ONE_BASED, "must be C index");
+  using index_type = typename CcsType_C::index_type;
+  using graph_type =
+      typename internal::BGL_UndirectedGraphTrait<index_type>::graph_type;
+
+  if (psmilu_verbose(PRE, opt)) psmilu_info("begin running RCM reordering...");
+
+  const graph_type graph = create_graph<IsSymm>(B);
+  const size_type  nv    = B.ncols();
+
+  Array<index_type> P(nv);
+  psmilu_error_if(P.status() == DATA_UNDEF, "memory allocation failed");
+  do {
+    // get property map
+    const typename boost::property_map<graph_type, boost::vertex_index_t>::type
+        index_map = boost::get(boost::vertex_index, graph);
+    // use vector's reversed iterator to enable RCM
+    std::vector<index_type> inv_perm(nv);
+    // call RCM
+    boost::cuthill_mckee_ordering(graph, inv_perm.rbegin());
+    for (size_type i(0); i < nv; ++i) P[i] = index_map[inv_perm[i]];
+  } while (false);
+  if (psmilu_verbose(PRE, opt)) psmilu_info("finish RCM reordering...");
+  return P;
+}
+
+}  // namespace psmilu
+
+#endif  // _PSMILU_BGL_RCM_HPP
