19be1d637f291dd87594cc42e87454a73f67c1d2
Qiao Chen <qiao.chen@stonybrook.edu>
2019-07-09 09:52:55 -0400

    converted old version interface

---
+++ src/hilucsi/version.h
@@ -0,0 +1,41 @@
+/*
+//@HEADER
+//----------------------------------------------------------------------------
+//                Copyright (C) 2019 The HILUCSI AUTHORS
+//----------------------------------------------------------------------------
+//@HEADER
+*/
+
+/*!
+ * \file HILUCSI hilucsi/utils/version.h
+ * \brief HILUCSI version header
+ * \authors Qiao,
+ */
+
+#ifndef _HILUCSI_UTILS_VERSION_H
+#define _HILUCSI_UTILS_VERSION_H
+
+/// \def HILUCSI_GLOBAL_VERSION
+/// \brief HILUCSI global version
+/// \ingroup itr
+
+/// \def HILUCSI_MAJOR_VERSION
+/// \brief HILUCSI major version
+/// \ingroup itr
+
+/// \def HILUCSI_MINOR_VERSION
+/// \brief HILUCSI minor version
+/// \ingroup itr
+
+/// \def HILUCSI_VERSION
+/// \brief HILUCSI uses three-digit version system, i.e. \a global.major.minor
+/// \ingroup itr
+
+#define HILUCSI_GLOBAL_VERSION 1
+#define HILUCSI_MAJOR_VERSION 0
+#define HILUCSI_MINOR_VERSION 0
+#define HILUCSI_VERSION                                        \
+  (100 * HILUCSI_GLOBAL_VERSION + 10 * HILUCSI_MAJOR_VERSION + \
+   HILUCSI_MINOR_VERSION)
+
+#endif /* _HILUCSI_UTILS_VERSION_H */
