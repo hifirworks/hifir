e5705c278c5d12630a4d7692b06b544d644f457c
Qiao Chen <qiao.chen@stonybrook.edu>
2019-07-09 12:40:17 -0400

    added dense structure and its tests

---
+++ tests/test_setting_opts.cpp
@@ -0,0 +1,53 @@
+//@HEADER
+//----------------------------------------------------------------------------
+//                Copyright (C) 2019 The HILUCSI AUTHORS
+//----------------------------------------------------------------------------
+//@HEADER
+
+#include "common.hpp"
+// line break to avoid sorting
+#include "hilucsi/Options.h"
+
+#include <gtest/gtest.h>
+
+using namespace hilucsi;
+
+const RandIntGen  i_rand(0, 100);
+const RandRealGen r_rand(0.0, 100.0);
+
+#define MUST_SUCCESS ASSERT_FALSE
+#define MUST_FAIL ASSERT_TRUE
+
+TEST(OPT, set) {
+  Options      opt;
+  const double tau_L = r_rand(), tau_U = r_rand(), tau_d = r_rand(),
+               tau_kappa = r_rand();
+  const int    alpha_L = i_rand(), alpha_U = i_rand();
+  const double rho = r_rand(), c_d = r_rand(), c_h = r_rand();
+  const int    N = i_rand(), verbose = i_rand();
+  MUST_SUCCESS(set_option_attr("tau_L", tau_L, opt));
+  MUST_SUCCESS(set_option_attr("tau_U", tau_U, opt));
+  MUST_SUCCESS(set_option_attr("tau_d", tau_d, opt));
+  MUST_SUCCESS(set_option_attr("tau_kappa", tau_kappa, opt));
+  MUST_SUCCESS(set_option_attr("alpha_L", alpha_L, opt));
+  MUST_SUCCESS(set_option_attr("alpha_U", alpha_U, opt));
+  MUST_SUCCESS(set_option_attr("rho", rho, opt));
+  MUST_SUCCESS(set_option_attr("c_d", c_d, opt));
+  MUST_SUCCESS(set_option_attr("c_h", c_h, opt));
+  MUST_SUCCESS(set_option_attr("N", N, opt));
+  MUST_SUCCESS(set_option_attr("verbose", verbose, opt));
+
+  ASSERT_EQ(opt.tau_L, tau_L);
+  ASSERT_EQ(opt.tau_U, tau_U);
+  ASSERT_EQ(opt.tau_d, tau_d);
+  ASSERT_EQ(opt.tau_kappa, tau_kappa);
+  ASSERT_EQ(opt.alpha_L, alpha_L);
+  ASSERT_EQ(opt.alpha_U, alpha_U);
+  ASSERT_EQ(opt.rho, rho);
+  ASSERT_EQ(opt.c_d, c_d);
+  ASSERT_EQ(opt.c_h, c_h);
+  ASSERT_EQ(opt.N, N);
+  ASSERT_EQ(opt.verbose, verbose);
+
+  MUST_FAIL(set_option_attr("foobar", 1, opt));
+}
---
+++ tests/test_sparse2dense.cpp
@@ -0,0 +1,85 @@
+//@HEADER
+//----------------------------------------------------------------------------
+//                Copyright (C) 2019 The HILUCSI AUTHORS
+//----------------------------------------------------------------------------
+//@HEADER
+
+#include "common.hpp"
+// line break to avoid sorting
+#include "hilucsi/ds/Array.hpp"
+#include "hilucsi/ds/CompressedStorage.hpp"
+#include "hilucsi/ds/DenseMatrix.hpp"
+
+#include <gtest/gtest.h>
+
+using namespace hilucsi;
+
+const static RandIntGen  i_rand(0, 100);
+const static RandRealGen r_rand(1.0, 2.0);
+
+using dense_t = DenseMatrix<double>;
+
+TEST(S2D, core) {
+  typedef CRS<double, int> crs_t;
+  crs_t                    crs1;
+  const int                nrows(i_rand() + 1), ncols(i_rand() + 1);
+  std::cout << "c-test, (nrows,ncols)=(" << nrows << ',' << ncols << ")\n";
+  crs1.resize(nrows, ncols);
+
+  auto                          mat_ref = create_mat<double>(nrows, ncols);
+  std::vector<std::vector<int>> col_inds(nrows);
+  std::vector<bool>             tags(ncols, false);
+  for (int i = 0; i < nrows; ++i) {
+    int nnz = i_rand() % ncols;
+    if (!nnz) nnz = (i_rand() % 2) ? 0 : ncols;
+    int counts = 0, guard = 0;
+    for (;;) {
+      if (counts == nnz || guard > 2 * nnz) break;
+      const int idx = i_rand() % ncols;
+      ++guard;
+      if (tags[idx]) continue;
+      ++counts;
+      tags[idx] = true;
+      col_inds[i].push_back(idx);
+    }
+    std::sort(col_inds[i].begin(), col_inds[i].end());
+    for (const auto j : col_inds[i]) tags[j] = false;
+  }
+  // construct sparse matrix
+  crs1.begin_assemble_rows();
+  for (int i = 0; i < nrows; ++i) {
+    for (const auto col : col_inds[i]) mat_ref[i][col] = r_rand();
+    crs1.push_back_row(i, col_inds[i].cbegin(), col_inds[i].cend(), mat_ref[i]);
+    ASSERT_EQ(crs1.nnz_in_row(i), col_inds[i].size());
+  }
+  crs1.end_assemble_rows();
+
+  auto mat(dense_t::from_sparse(crs1));
+  for (int i = 0; i < nrows; ++i)
+    for (int j = 0; j < ncols; ++j)
+      ASSERT_EQ(mat(i, j), mat_ref[i][j])
+          << "(i,j)=(" << i << ',' << j << ", failed!\n";
+
+  typedef CCS<double, int> ccs_t;
+  ccs_t                    ccs(nrows, ncols);
+  std::vector<int>         inds;
+  std::vector<double>      vals(nrows);
+  ccs.begin_assemble_cols();
+  for (int j = 0; j < ncols; ++j) {
+    for (int i = 0; i < nrows; ++i)
+      if (mat_ref[i][j] != 0.0) {
+        inds.push_back(i);
+        vals[i] = mat_ref[i][j];
+      }
+    ccs.push_back_col(j, inds.cbegin(), inds.cend(), vals);
+    ASSERT_EQ(ccs.nnz_in_col(j), inds.size());
+    inds.clear();
+  }
+  ccs.end_assemble_cols();
+
+  mat.copy_sparse(ccs);
+  for (int i = 0; i < nrows; ++i)
+    for (int j = 0; j < ncols; ++j)
+      ASSERT_EQ(mat(i, j), mat_ref[i][j])
+          << "(i,j)=(" << i << ',' << j << ", failed!\n";
+}
