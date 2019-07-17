55cad8cded4a12c324ae6f387d4c2bf9dead7d83
Qiao Chen <qiao.chen@stonybrook.edu>
2019-07-17 09:28:31 -0400

    fixed wrong copyright title

--- tests/test_native_io.cpp
+++ tests/test_native_io.cpp
@@ -1,6 +1,6 @@
 //@HEADER
 //----------------------------------------------------------------------------
-//                Copyright (C) 2019 The PSMILU AUTHORS
+//                Copyright (C) 2019 The HILUCSI AUTHORS
 //----------------------------------------------------------------------------
 //@HEADER
 
@@ -14,31 +14,29 @@
 
 using namespace hilucsi;
 
-TEST(IO, c) {
-  {
-    const int n  = RandIntGen(100, 300)();
-    using crs_t  = CRS<double, int>;
-    const auto A = gen_rand_sparse<crs_t>(n, n);
-    write_bin("foo.bin", A, 0u);
-    crs_t B;
-    auto  m = read_bin("foo.bin", B);
-    ASSERT_EQ(m, 0u);
-    const auto Ad = convert2dense(A), Bd = convert2dense(B);
-    COMPARE_MATS(Ad, Bd);
-    using crs2_t = CRS<double, long>;
-    crs2_t C;
-    m             = read_bin("foo.bin", C);
-    const auto Cd = convert2dense(C);
-    COMPARE_MATS(Cd, Ad);
-    using ccs_t = CCS<double, int>;
-    ccs_t E;
-    read_bin("foo.bin", E);
-    const auto Ed = convert2dense(E);
-    COMPARE_MATS(Ed, Ad);
-    using ccs2_t = CCS<float, int>;
-    ccs2_t F;
-    read_bin("foo.bin", F);
-    const auto Fd = convert2dense(F);
-    COMPARE_MATS_TOL(Fd, Ad, 1e-6);
-  }
+TEST(IO, core) {
+  const int n  = RandIntGen(100, 300)();
+  using crs_t  = CRS<double, int>;
+  const auto A = gen_rand_sparse<crs_t>(n, n);
+  write_bin("foo.bin", A, 0u);
+  crs_t B;
+  auto  m = read_bin("foo.bin", B);
+  ASSERT_EQ(m, 0u);
+  const auto Ad = convert2dense(A), Bd = convert2dense(B);
+  COMPARE_MATS(Ad, Bd);
+  using crs2_t = CRS<double, long>;
+  crs2_t C;
+  m             = read_bin("foo.bin", C);
+  const auto Cd = convert2dense(C);
+  COMPARE_MATS(Cd, Ad);
+  using ccs_t = CCS<double, int>;
+  ccs_t E;
+  read_bin("foo.bin", E);
+  const auto Ed = convert2dense(E);
+  COMPARE_MATS(Ed, Ad);
+  using ccs2_t = CCS<float, int>;
+  ccs2_t F;
+  read_bin("foo.bin", F);
+  const auto Fd = convert2dense(F);
+  COMPARE_MATS_TOL(Fd, Ad, 1e-6);
 }
--- tests/test_native_io_ascii.cpp
+++ tests/test_native_io_ascii.cpp
@@ -1,6 +1,6 @@
 //@HEADER
 //----------------------------------------------------------------------------
-//                Copyright (C) 2019 The PSMILU AUTHORS
+//                Copyright (C) 2019 The HILUCSI AUTHORS
 //----------------------------------------------------------------------------
 //@HEADER
 
@@ -15,39 +15,35 @@
 using namespace hilucsi;
 
 TEST(IO, crs) {
-  const int n = RandIntGen(100, 300)();
-  {
-    using crs_t  = CRS<double, int>;
-    const auto A = gen_rand_sparse<crs_t>(n, n);
-    A.write_ascii("foo1.hilucsi");
-    crs_t B;
-    B.read_ascii("foo1.hilucsi");
-    const auto Ad = convert2dense(A);
-    const auto Bd = convert2dense(B);
-    COMPARE_MATS(Ad, Bd);
-    crs_t::other_type D;
-    D.read_ascii("foo1.hilucsi");
-    const auto Dd = convert2dense(D);
-    COMPARE_MATS(Ad, Dd);
-  }
+  const int n  = RandIntGen(100, 300)();
+  using crs_t  = CRS<double, int>;
+  const auto A = gen_rand_sparse<crs_t>(n, n);
+  A.write_ascii("foo1.hilucsi");
+  crs_t B;
+  B.read_ascii("foo1.hilucsi");
+  const auto Ad = convert2dense(A);
+  const auto Bd = convert2dense(B);
+  COMPARE_MATS(Ad, Bd);
+  crs_t::other_type D;
+  D.read_ascii("foo1.hilucsi");
+  const auto Dd = convert2dense(D);
+  COMPARE_MATS(Ad, Dd);
 }
 
 TEST(IO, ccs) {
-  const int n = RandIntGen(100, 300)();
-  {
-    using ccs_t  = CCS<double, int>;
-    const auto A = gen_rand_sparse<ccs_t>(n, n);
-    A.write_ascii("foo2.hilucsi");
-    ccs_t B;
-    B.read_ascii("foo2.hilucsi");
+  const int n  = RandIntGen(100, 300)();
+  using ccs_t  = CCS<double, int>;
+  const auto A = gen_rand_sparse<ccs_t>(n, n);
+  A.write_ascii("foo2.hilucsi");
+  ccs_t B;
+  B.read_ascii("foo2.hilucsi");
 
-    const auto Ad = convert2dense(A);
-    const auto Bd = convert2dense(B);
-    COMPARE_MATS(Ad, Bd);
-    ccs_t::other_type D;
+  const auto Ad = convert2dense(A);
+  const auto Bd = convert2dense(B);
+  COMPARE_MATS(Ad, Bd);
+  ccs_t::other_type D;
 
-    D.read_ascii("foo2.hilucsi");
-    const auto Dd = convert2dense(D);
-    COMPARE_MATS(Ad, Dd);
-  }
+  D.read_ascii("foo2.hilucsi");
+  const auto Dd = convert2dense(D);
+  COMPARE_MATS(Ad, Dd);
 }
