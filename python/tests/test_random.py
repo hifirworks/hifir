a52d8a64ecd97380fd09f0f0ca40887df246babc
Qiao Chen <qiao.chen@stonybrook.edu>
2019-07-21 14:04:27 -0400

    updated the test with cleaner interface

--- python/tests/test_random.py
+++ python/tests/test_random.py
@@ -3,16 +3,15 @@
 #                Copyright (C) 2019 The HILUCSI AUTHORS
 # ----------------------------------------------------------------------------
 
-from hilucsi4py import *
+from hilucsi4py import FGMRES
 from scipy.sparse import random
 import numpy as np
 
 
 def test_random():
     A = random(10, 10, 0.5)
-    M = HILUCSI()
-    M.factorize(A)
+    solver = FGMRES()
+    solver.M.factorize(A)
     b = A * np.ones(10)
-    solver = FGMRES(M)
     x, _ = solver.solve(A, b)
     assert np.linalg.norm(x - 1) <= 1e-6
