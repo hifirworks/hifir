3dc060a161a924fcf53ec9a02ece5af513366fc3
Qiao Chen <qiao.chen@stonybrook.edu>
2019-07-21 11:41:30 -0400

    reoworked GMRES based rst KSP class

--- src/hilucsi/ksp/FGMRES.hpp
+++ src/hilucsi/ksp/FGMRES.hpp
@@ -13,230 +13,25 @@
 
 #include <cmath>
 #include <limits>
-#include <memory>
 #include <utility>
 
 #include "hilucsi/ksp/common.hpp"
-#include "hilucsi/utils/log.hpp"
 #include "hilucsi/utils/math.hpp"
 
 namespace hilucsi {
 namespace ksp {
-namespace internal {
-
-/*!
- * \addtogroup gmres
- * @{
- */
-
-/// \class JacobiBase
-/// \tparam Child child jacobi iteration object, either \ref Jacobi or
-///         \ref ChebyshevJacobi
-/// \tparam MType "preconditioner" operator type, see \ref HILUCSI
-/// \brief generic base class for Jacobi iterations
-template <class Child, class MType>
-class JacobiBase {
-  /// \brief helper for casting to \a Child
-  inline const Child &_this() const {
-    return static_cast<const Child &>(*this);
-  }
-
- public:
-  typedef MType                           M_type;      ///< preconditioner type
-  typedef typename M_type::array_type     array_type;  ///< value array type
-  typedef typename array_type::value_type value_type;  ///< value type
-  typedef typename array_type::size_type  size_type;   ///< size type
-
-  JacobiBase() = delete;
-
-  /// \brief constructor with reference to preconditioner
-  /// \param[in] M preconditioner, see \ref HILUCSI
-  explicit JacobiBase(const M_type &M) : _M(M), _xk(M.ncols()), _r(M.nrows()) {
-    if (M.nrows() && _xk.empty()) hilucsi_error("memory allocation failed");
-    if (M.ncols() && _r.empty()) hilucsi_error("memory allocation failed");
-  }
-
-  /// \brief check if the M's address is the same
-  /// \param[in] M multilevel ILU
-  inline bool is_same_M(const M_type &M) const { return &_M == &M; }
-
-  /// \brief generic implementation of Jacobi iterations with explicit steps
-  /// \tparam Matrix matrix type, see \ref CRS or \ref CCS
-  /// \param[in] A input matrix
-  /// \param[in] b right-hand side vector
-  /// \param[in] N number of iterations
-  /// \param[out] x0 solution of Jacobi after \a N iterations
-  /// \return if return \a true, then solving failed
-  template <class Matrix>
-  inline bool solve(const Matrix &A, const array_type &b, const size_type N,
-                    array_type &x0) const {
-    // The Jacobi iteration reads:
-    //    A*x=b
-    //    (D+A-D)*x=b
-    //    D*x_{k+1}=b-(A-D)*x_k
-    //    x_{k+1}=inv(D)*(b-(A-D)*x_k)
-    //    x_{k+1}=inv(D)*(b-A*x_k+D*x_k)
-    //    x_{k+1}=inv(D)*res_k+x_k
-    if (_M.nrows() != A.nrows() || _M.ncols() != A.ncols()) return true;
-    if (b.size() != A.nrows() || A.ncols() != x0.size()) return true;
-    size_type       iters(0);
-    const size_type n(b.size());
-    auto &          x = x0;
-    std::fill(x.begin(), x.end(), value_type(0));
-    for (; iters < N; ++iters) {
-      // copy rhs to x
-      std::copy(x.cbegin(), x.cend(), _xk.begin());
-      if (iters) {
-        // compute A*xk=x
-        A.mv(_xk, x);
-        // compute residual x=b-x
-        for (size_type i(0); i < n; ++i) x[i] = b[i] - x[i];
-      } else
-        std::copy_n(b.cbegin(), n, x.begin());
-      // compute inv(M)*x=r
-      _M.solve(x, _r);
-      // call child's post process to update to current solution
-      _this()._update(iters, x);
-    }
-    return false;
-  }
-
- protected:
-  const M_type &     _M;   ///< reference to preconditioner
-  mutable array_type _xk;  ///< previous solution
-  mutable array_type _r;   ///< inv(M)*residual
-};
-
-/// \class Jacobi
-/// \brief regular Jacobi iterations
-/// \tparam MType "preconditioner" operator type, see \ref HILUCSI
-template <class MType>
-class Jacobi : public internal::JacobiBase<Jacobi<MType>, MType> {
-  using _base = internal::JacobiBase<Jacobi<MType>, MType>;  ///< base
-  friend _base;
-
- public:
-  typedef typename _base::M_type     M_type;      ///< preconditioner type
-  typedef typename _base::array_type array_type;  ///< value array type
-  typedef typename _base::size_type  size_type;   ///< value type
-
-  Jacobi() = delete;
-
-  /// \brief constructor with reference to preconditioner
-  /// \param[in] M preconditioner, see \ref HILUCSI
-  explicit Jacobi(const M_type &M) : _base(M) {}
-
- protected:
-  using _base::_r;
-  using _base::_xk;
-
-  /// \brief default post processing for static inheritance
-  /// \param[out] x solution of step k
-  inline void _update(const size_type, array_type &x) const {
-    const size_type n = x.size();
-    for (size_type i(0); i < n; ++i) x[i] = _r[i] + _xk[i];
-  }
-};
-
-/// \class ChebyshevJacobi
-/// \brief Jacobi iterations with Chebyshev accelerations
-/// \tparam MType "preconditioner" operator type, see \ref HILUCSI
-template <class MType>
-class ChebyshevJacobi
-    : public internal::JacobiBase<ChebyshevJacobi<MType>, MType> {
-  using _base = internal::JacobiBase<ChebyshevJacobi<MType>, MType>;  ///< base
-  friend _base;
-
- public:
-  typedef typename _base::M_type     M_type;      ///< preconditioner type
-  typedef typename _base::array_type array_type;  ///< value array type
-  typedef typename _base::size_type  size_type;   ///< size type
-  typedef typename _base::value_type value_type;  ///< value type
-
-  ChebyshevJacobi() = delete;
-
-  /// \brief constructor with preconditioner and largest/smallest eig est
-  /// \param[in] M reference to preconditioner
-  /// \param[in] lamb1 largest eigenvalue estimation
-  /// \param[in] lamb2 smallest eigenvalue estimation
-  ChebyshevJacobi(const M_type &M, const value_type lamb1,
-                  const value_type lamb2)
-      : _base(M), _rho(2), _xkk(M.ncols()) {
-    if (M.ncols() && _xkk.empty()) hilucsi_error("memory allocation failed");
-    _compute_coeffs(lamb1, lamb2);
-  }
-
- protected:
-  mutable value_type _rho;
-  mutable array_type _xkk;
-  value_type         _gamma;
-  value_type         _sigma2;
-  using _base::_r;
-  using _base::_xk;
-
- protected:
-  /// \brief compute essential coefficients
-  inline void _compute_coeffs(const value_type l1, const value_type l2) {
-    _gamma  = 2. / (2. - l1 - l2);
-    _sigma2 = 0.5 * _gamma * (l1 - l2);
-    _sigma2 = _sigma2 * _sigma2;
-  }
-
-  /// \brief default post processing for static inheritance
-  /// \param[in] iter current inner iteration counts
-  /// \param[out] x solution of step k
-  inline void _update(const size_type iter, array_type &x) const {
-    const size_type n = x.size();
-    for (size_type i(0); i < n; ++i) x[i] = _xk[i] + _gamma * _r[i];
-    if (iter) {
-      // NOTE _rho is initialized as 2, thus it valids for iter==1
-      _rho            = 1. / (1. - 0.25 * _sigma2 * _rho);
-      const auto beta = 1. - _rho;
-      for (size_type i(0); i < n; ++i) x[i] = _rho * x[i] + beta * _xkk[i];
-    }
-    // update error
-    for (size_type i(0); i < n; ++i) _r[i] = x[i] - _xk[i];
-    std::copy(_xk.cbegin(), _xk.cend(), _xkk.begin());
-  }
-};
-
-/// \class DummyJacobi
-/// \brief dummy "Jacobi" that has the same solve interface as, say, \ref Jacobi
-/// \tparam MType "preconditioner" operator type, see \ref HILUCSI
-template <class MType>
-class DummyJacobi {
- public:
-  typedef MType                       M_type;
-  typedef typename M_type::array_type array_type;
-  typedef typename M_type::size_type  size_type;
-
-  DummyJacobi() = delete;
-
-  explicit DummyJacobi(const M_type &M) : _M(M) {}
-
-  template <class Matrix>
-  inline bool solve(const Matrix &, const array_type &b, const size_type,
-                    array_type &x0) const {
-    _M.solve(b, x0);
-    return false;
-  }
-
- protected:
-  const M_type &_M;
-};
-
-/*!
- * @}
- */
-
-}  // namespace internal
 
 /// \class FGMRES
 /// \tparam MType preconditioner type, see \ref HILUCSI
 /// \brief flexible GMRES implementation
 /// \ingroup gmres
 template <class MType>
-class FGMRES {
+class FGMRES : public internal::KSP<FGMRES<MType>, MType> {
+ protected:
+  using _base = internal::KSP<FGMRES<MType>, MType>;  ///< base
+  // grant friendship
+  friend _base;
+
  public:
   typedef MType                           M_type;      ///< preconditioner
   typedef typename M_type::array_type     array_type;  ///< value array
@@ -249,23 +44,18 @@ class FGMRES {
   static_assert(std::is_floating_point<scalar_type>::value,
                 "must be floating point type");
 
-  enum {
-    TRADITION = 0,     ///< traditional kernel
-    JACOBI,            ///< with inner jacobi iterations
-    CHEBYSHEV_JACOBI,  ///< with inner Jacobi plus Chebyshev accelerations
-  };
-
   /// \brief get the solver name
   inline static const char *repr() { return "FGMRES"; }
 
-  scalar_type rtol = DefaultSettings<value_type>::rtol;
-  ///< relative convergence tolerance
-  int       restart = 30;  ///< restart
-  size_type maxit   = DefaultSettings<value_type>::max_iters;
-  ///< max numer of iterations
-  size_type  max_inners = 4u;   ///< maximum inner iterations
-  value_type lamb1      = 0.9;  ///< est of largest eigenvalue
-  value_type lamb2      = 0.0;  ///< est of smallest eigenvalue
+  using _base::rtol;
+
+  using _base::maxit;
+
+  using _base::inner_steps;
+  using _base::lamb1;
+  using _base::lamb2;
+
+  int restart = 30;  ///< restart
 
   FGMRES() = default;
 
@@ -280,28 +70,11 @@ class FGMRES {
       const scalar_type       rel_tol = DefaultSettings<value_type>::rtol,
       const int               rs      = 30,
       const size_type max_iters       = DefaultSettings<value_type>::max_iters,
-      const size_type max_inner_steps = 4u)
-      : _M(M),
-        rtol(rel_tol),
-        restart(rs),
-        maxit(max_iters),
-        max_inners(max_inner_steps) {
-    _check_pars();
+      const size_type max_inner_steps =
+          DefaultSettings<value_type>::inner_steps)
+      : restart(rs), _base(M, rel_tol, max_iters, max_inner_steps) {
     if (_M && _M->nrows()) _ensure_data_capacities(_M->nrows());
   }
-
-  /// \brief set preconditioner
-  /// \param[in] M multilevel ILU preconditioner
-  inline void set_M(std::shared_ptr<M_type> M) {
-    _M = M;  // increment internal reference counter
-    if (_M && _M->nrows()) _ensure_data_capacities(_M->nrows());
-  }
-
-  /// \brief get preconditioner
-  inline std::shared_ptr<M_type> get_M() const { return _M; }
-
-  /// \brief get residual array
-  inline const array_type &resids() const { return _resids; }
 
   /// \brief solve with \ref _M as traditional preconditioner
   /// \tparam Matrix user input type, see \ref CRS and \ref CCS
@@ -321,7 +94,7 @@ class FGMRES {
     const static hilucsi::internal::DummyErrorStreamer Dummy_cerr;
 
     if (_validate(A, b, x)) return std::make_pair(INVALID_ARGS, size_type(0));
-    if (verbose) _show("tradition", with_init_guess);
+    if (verbose) _base::_show("tradition", with_init_guess, restart);
     if (!with_init_guess) std::fill(x.begin(), x.end(), value_type(0));
     const internal::DummyJacobi<M_type> M(*_M);
     if (verbose) hilucsi_info("Calling traditional GMRES kernel...");
@@ -348,12 +121,12 @@ class FGMRES {
     const static hilucsi::internal::DummyErrorStreamer Dummy_cerr;
 
     if (_validate(A, b, x)) return std::make_pair(INVALID_ARGS, size_type(0));
-    if (verbose) _show("Jacobi", with_init_guess);
+    if (verbose) _base::_show("Jacobi", with_init_guess, restart);
     const internal::Jacobi<M_type> M(*_M);
     if (!with_init_guess) std::fill(x.begin(), x.end(), value_type(0));
     return verbose
-               ? _solve(A, M, b, max_inners, !with_init_guess, x, Cout, Cerr)
-               : _solve(A, M, b, max_inners, !with_init_guess, x,
+               ? _solve(A, M, b, inner_steps, !with_init_guess, x, Cout, Cerr)
+               : _solve(A, M, b, inner_steps, !with_init_guess, x,
                         Dummy_streamer, Dummy_cerr);
   }
 
@@ -376,14 +149,14 @@ class FGMRES {
 
     if (_validate(A, b, x)) return std::make_pair(INVALID_ARGS, size_type(0));
     if (verbose) {
-      _show("Chebyshev", with_init_guess);
+      _base::_show("Chebyshev", with_init_guess, restart);
       hilucsi_warning("Chebyshev Jacobi with GMRES is experiemental...");
     }
     const internal::ChebyshevJacobi<M_type> M(*_M, lamb1, lamb2);
     if (!with_init_guess) std::fill(x.begin(), x.end(), value_type(0));
     return verbose
-               ? _solve(A, M, b, max_inners, !with_init_guess, x, Cout, Cerr)
-               : _solve(A, M, b, max_inners, !with_init_guess, x,
+               ? _solve(A, M, b, inner_steps, !with_init_guess, x, Cout, Cerr)
+               : _solve(A, M, b, inner_steps, !with_init_guess, x,
                         Dummy_streamer, Dummy_cerr);
   }
 
@@ -416,23 +189,25 @@ class FGMRES {
   }
 
  protected:
-  std::shared_ptr<M_type> _M;  ///< preconditioner operator
-  mutable array_type      _y;
-  mutable array_type      _R;
-  mutable array_type      _Q;  ///< Q space
-  mutable array_type      _Z;
-  mutable array_type      _J;
-  mutable array_type      _v;
-  mutable array_type      _resids;  ///< residual history
-  mutable array_type      _w;
+  /// \name workspace
+  /// Workspace for GMRES algorithm
+  /// @{
+  mutable array_type _y;
+  mutable array_type _R;
+  mutable array_type _Q;  ///< Q space
+  mutable array_type _Z;
+  mutable array_type _J;
+  mutable array_type _v;
+  mutable array_type _w;
+  /// @}
+  using _base::_M;
+  using _base::_resids;
 
  protected:
   /// \brief check and assign any illegal parameters to default setting
   inline void _check_pars() {
-    if (rtol <= 0) rtol = DefaultSettings<value_type>::rtol;
     if (restart <= 0) restart = 30;
-    if (maxit == 0u) maxit = DefaultSettings<value_type>::max_iters;
-    if (max_inners == 0u) max_inners = 4u;
+    _base::_check_pars();
   }
 
   /// \brief ensure internal buffer sizes
@@ -444,34 +219,16 @@ class FGMRES {
     _Z.resize(_Q.size());
     _J.resize(2 * restart);
     _v.resize(n);
-    _resids.reserve(maxit + 1);
-    _resids.resize(1);
     _w.resize(std::max(n, size_type(restart)));
+    _base::_init_resids();
   }
 
   /// \brief validation checking
   template <class Matrix>
   inline bool _validate(const Matrix &A, const array_type &b,
                         const array_type &x) const {
-    if (!_M || _M->empty()) return true;
-    if (_M->nrows() != A.nrows()) return true;
-    if (b.size() != A.nrows()) return true;
-    if (b.size() != x.size()) return true;
-    if (rtol <= 0.0) return true;
     if (restart <= 0) return true;
-    if (maxit == 0u) return true;
-    if (max_inners == 0u) return true;
-    return false;
-  }
-
-  /// \brief show information
-  /// \param[in] kernel kernel name
-  /// \param[in] with_init_guess solve with initial guess flag
-  inline void _show(const char *kernel, const bool with_init_guess) const {
-    hilucsi_info(
-        "- FGMRES -\nrtol=%g\nrestart=%d\nmaxiter=%zd\nkernel: %s\ninit-guess: "
-        "%s\n",
-        rtol, restart, maxit, kernel, (with_init_guess ? "yes" : "no"));
+    return _base::_validate(A, b, x);
   }
 
   /// \brief low level solve kernel
@@ -550,7 +307,8 @@ class FGMRES {
         A.mv(_w, _v);
         if (n < (size_type)restart) _w.resize(restart);
         for (size_type k = 0u; k <= j; ++k) {
-          auto itr       = _Q.cbegin() + k * n;
+          auto itr = _Q.cbegin() + k * n;
+
           _w[k]          = inner(_v, itr);
           const auto tmp = _w[k];
           for (size_type i = 0u; i < n; ++i) _v[i] -= tmp * itr[i];
