dca1a5264263755b9111897cce0d5cecba6bb6ef
Qiao Chen <qiao.chen@stonybrook.edu>
2019-07-21 14:17:06 -0400

    added qmrcgstab test

---
+++ python/tests/test_fqmrcgstab.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
+#                Copyright (C) 2019 The HILUCSI AUTHORS
+# ----------------------------------------------------------------------------
+
+from hilucsi4py import FQMRCGSTAB
+from scipy.sparse import random
+import numpy as np
+
+
+def test_random():
+    A = random(10, 10, 0.5)
+    solver = FQMRCGSTAB()
+    solver.M.factorize(A)
+    b = A * np.ones(10)
+    x, _ = solver.solve(A, b)
+    assert np.linalg.norm(x - 1) <= 1e-6
