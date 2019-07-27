79d5cfd21c28e05c78c11efbd19838595b3de627
Qiao Chen <qiao.chen@stonybrook.edu>
2019-07-27 15:17:55 -0400

    added tests for mixed presision

---
+++ python/tests/test_fqmrcgstab_mixed.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
+#                Copyright (C) 2019 The HILUCSI AUTHORS
+# ----------------------------------------------------------------------------
+
+from hilucsi4py import FQMRCGSTAB_Mixed
+from scipy.sparse import random
+import numpy as np
+
+
+def test_random():
+    A = random(10, 10, 0.5)
+    solver = FQMRCGSTAB_Mixed()
+    solver.M.factorize(A)
+    b = A * np.ones(10)
+    x, _ = solver.solve(A, b)
+    res = np.linalg.norm(x - 1) / np.linalg.norm(b)
+    assert res <= 1e-6
+    assert abs(res - solver.resids[-1]) <= 1e-6
