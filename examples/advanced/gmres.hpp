d6bd6d74bf362218b92340a938e5f02df7ec82b1
Qiao Chen <qiao.chen@stonybrook.edu>
2021-07-28 12:03:19 -0400

    Extracted out the gmres solvers

---
+++ examples/advanced/gmres.hpp
@@ -0,0 +1,226 @@
+///////////////////////////////////////////////////////////////////////////////
+//                  This file is part of the HIFIR library                   //
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef _HIFIR_DEMOGMRES_HPP
+#define _HIFIR_DEMOGMRES_HPP
+
+#include <tuple>
+
+#include "../demo_utils.hpp"
+
+#define SUCCESS 0
+#define STAGNATED 1
+#define DIVERGED 2
+
+// Right-preconditioned GMRES with HIF preconditioner for consistent system
+// using MGS for Arnoldi
+template <class MatType, class ArrayType, class PrecType>
+inline std::tuple<ArrayType, int, int> gmres_hif(
+    const MatType &A, const ArrayType &b, const PrecType &M, const int restart,
+    const double rtol, const int maxit, const int verbose) {
+  using size_type = typename ArrayType::size_type;
+
+  int             iter(0), flag(SUCCESS);
+  const size_type n     = b.size();
+  const auto      beta0 = hif::norm2(b);
+
+  // create solution vector, starting with all zeros
+  ArrayType x(n);
+  std::fill_n(x.begin(), n, 0.0);
+  // quick return if possible
+  if (beta0 == 0.0) return std::make_tuple(x, SUCCESS, iter);
+
+  // create local workspace
+  ArrayType v(n), w(n), y(restart + 1), w2(restart);
+  hif::DenseMatrix<typename ArrayType::value_type> Q(n, restart),
+      R(restart, restart), J(restart, 2);
+
+  const int max_outer_iters = std::ceil((double)maxit / restart);
+  double    resid(1);  // residual norm
+  for (int it_outer = 0; it_outer < max_outer_iters; ++it_outer) {
+    if (verbose > 1) hif_info(" Enter outer iteration %d...", it_outer + 1);
+    // initial residual
+    if (iter) {
+      A.multiply(x, v);                                       // A*x
+      for (size_type i = 0u; i < n; ++i) v[i] = b[i] - v[i];  // b-A*x
+    } else
+      std::copy_n(b.cbegin(), n, v.begin());
+    const double beta = hif::norm2(v);
+    y[0]              = beta;
+    for (size_type i = 0u; i < n; ++i) Q(i, 0) = v[i] / beta;
+    int j(0);  // inner counter
+    for (;;) {
+      std::copy(Q.col_cbegin(j), Q.col_cend(j), v.begin());
+      M.solve(v, w);     // multilevel triangular solve
+      A.multiply(w, v);  // matrix-vector
+
+      // Perform Gram-Schmidt orthogonalization
+      for (int k = 0u; k <= j; ++k) {
+        w2[k] = hif::inner(v, Q.col_cbegin(k));
+        for (size_type i = 0u; i < n; ++i) v[i] -= w2[k] * Q(i, k);
+      }
+      const auto v_norm2 = hif::norm2_sq(v);
+      const auto v_norm  = std::sqrt(v_norm2);
+      if (j + 1 < restart)
+        for (size_type i = 0u; i < n; ++i) Q(i, j + 1) = v[i] / v_norm;
+
+      // Perform Given's rotation to w2
+      for (int colJ = 0; colJ + 1 <= j; ++colJ) {
+        const auto tmp = w2[colJ];
+        w2[colJ]       = hif::conjugate(J(colJ, 0)) * tmp +
+                   hif::conjugate(J(colJ, 1)) * w2[colJ + 1];
+        w2[colJ + 1] = -J(colJ, 1) * tmp + J(colJ, 0) * w2[colJ + 1];
+      }
+      const auto rho = std::sqrt(hif::conjugate(w2[j]) * w2[j] + v_norm2);
+      J(j, 0)        = w2[j] / rho;
+      J(j, 1)        = v_norm / rho;
+      y[j + 1]       = -J(j, 1) * y[j];
+      y[j]           = hif::conjugate(J(j, 0)) * y[j];
+      w2[j]          = rho;
+      std::copy_n(w2.cbegin(), j + 1, R.col_begin(j));
+
+      // get residual
+      const auto resid_prev = resid;
+      resid                 = hif::abs(y[j + 1]) / beta0;  // current residual
+      if (resid >= resid_prev * (1.0 - 1e-8)) {
+        if (verbose > 1) hif_info("  Solver stagnated!");
+        flag = STAGNATED;
+        break;
+      } else if (iter >= maxit) {
+        if (verbose > 1) hif_info("  Reached maxit iteration limit %d", maxit);
+        flag = DIVERGED;
+        break;
+      }
+      ++iter;
+      if (verbose > 1)
+        hif_info("  At iteration %d, relative residual is %g.", iter, resid);
+      if (resid <= rtol || j + 1 >= restart) break;
+      ++j;
+    }  // inf loop
+    // backsolve
+    for (int k = j; k > -1; --k) {
+      y[k] /= R(k, k);
+      const auto tmp = y[k];
+      for (int i = k - 1; i > -1; --i) y[i] -= tmp * R(i, k);
+    }
+    // compute Q*y
+    std::fill_n(v.begin(), n, 0);
+    for (int i = 0; i <= j; ++i) {
+      const auto tmp = y[i];
+      for (size_type k = 0u; k < n; ++k) v[k] += tmp * Q(k, i);
+    }
+    // compute M solve
+    M.solve(v, w);
+    for (size_type k(0); k < n; ++k) x[k] += w[k];  // accumulate sol
+    if (resid <= rtol || flag != SUCCESS) break;
+  }
+  return std::make_tuple(x, flag, iter);
+}
+
+// Right-preconditioned FGMRES with HIFIR preconditioner
+// using MGS for Arnoldi
+template <class MatType, class ArrayType, class PrecType>
+inline std::tuple<ArrayType, int, int, int> fgmres_hifir(
+    const MatType &A, const ArrayType &b, const PrecType &M, const int restart,
+    const double rtol, const int maxit, const int verbose) {
+  using size_type = typename ArrayType::size_type;
+
+  int             iter(0), flag(SUCCESS), num_mv(0);
+  const size_type n     = b.size();
+  const auto      beta0 = hif::norm2(b);
+
+  // create solution vector, starting with all zeros
+  ArrayType x(n);
+  std::fill_n(x.begin(), n, 0.0);
+  // quick return if possible
+  if (beta0 == 0.0) return std::make_tuple(x, SUCCESS, iter, 0);
+
+  // create local workspace
+  ArrayType v(n), w(n), y(restart + 1), w2(restart);
+  hif::DenseMatrix<typename ArrayType::value_type> Q(n, restart), Z(n, restart),
+      R(restart, restart), J(restart, 2);
+
+  const int max_outer_iters = std::ceil((double)maxit / restart);
+  double    resid(1);  // residual norm
+  for (int it_outer = 0; it_outer < max_outer_iters; ++it_outer) {
+    if (verbose > 1) hif_info(" Enter outer iteration %d...", it_outer + 1);
+    // initial residual
+    if (iter) {
+      A.multiply(x, v);                                       // A*x
+      for (size_type i = 0u; i < n; ++i) v[i] = b[i] - v[i];  // b-A*x
+    } else
+      std::copy_n(b.cbegin(), n, v.begin());
+    const double beta = hif::norm2(v);
+    y[0]              = beta;
+    for (size_type i = 0u; i < n; ++i) Q(i, 0) = v[i] / beta;
+    int       j(0);  // inner counter
+    const int nirs = 1 << (int)it_outer;
+    for (;;) {
+      std::copy(Q.col_cbegin(j), Q.col_cend(j), v.begin());
+      M.hifir(A, v, nirs, w, false, size_type(0));
+      // stored the preconditioned vector
+      num_mv += nirs;
+      std::copy(w.cbegin(), w.cend(), Z.col_begin(j));
+      A.multiply(w, v);  // matrix-vector
+
+      // Perform Gram-Schmidt orthogonalization
+      for (int k = 0u; k <= j; ++k) {
+        w2[k] = hif::inner(v, Q.col_cbegin(k));
+        for (size_type i = 0u; i < n; ++i) v[i] -= w2[k] * Q(i, k);
+      }
+      const auto v_norm2 = hif::norm2_sq(v);
+      const auto v_norm  = std::sqrt(v_norm2);
+      if (j + 1 < restart)
+        for (size_type i = 0u; i < n; ++i) Q(i, j + 1) = v[i] / v_norm;
+
+      // Perform Given's rotation to w2
+      for (int colJ = 0; colJ + 1 <= j; ++colJ) {
+        const auto tmp = w2[colJ];
+        w2[colJ]       = hif::conjugate(J(colJ, 0)) * tmp +
+                   hif::conjugate(J(colJ, 1)) * w2[colJ + 1];
+        w2[colJ + 1] = -J(colJ, 1) * tmp + J(colJ, 0) * w2[colJ + 1];
+      }
+      const auto rho = std::sqrt(hif::conjugate(w2[j]) * w2[j] + v_norm2);
+      J(j, 0)        = w2[j] / rho;
+      J(j, 1)        = v_norm / rho;
+      y[j + 1]       = -J(j, 1) * y[j];
+      y[j]           = hif::conjugate(J(j, 0)) * y[j];
+      w2[j]          = rho;
+      std::copy_n(w2.cbegin(), j + 1, R.col_begin(j));
+
+      // get residual
+      const auto resid_prev = resid;
+      resid                 = hif::abs(y[j + 1]) / beta0;  // current residual
+      if (resid >= resid_prev * (1.0 - 1e-8)) {
+        if (verbose > 1) hif_info("  Solver stagnated!");
+        flag = STAGNATED;
+        break;
+      } else if (iter >= maxit) {
+        if (verbose > 1) hif_info("  Reached maxit iteration limit %d", maxit);
+        flag = DIVERGED;
+        break;
+      }
+      ++iter;
+      if (verbose > 1)
+        hif_info("  At iteration %d, relative residual is %g.", iter, resid);
+      if (resid <= rtol || j + 1 >= restart) break;
+      ++j;
+    }  // inf loop
+    // backsolve
+    for (int k = j; k > -1; --k) {
+      y[k] /= R(k, k);
+      const auto tmp = y[k];
+      for (int i = k - 1; i > -1; --i) y[i] -= tmp * R(i, k);
+    }
+    // compute Q*y
+    for (int i = 0; i <= j; ++i) {
+      const auto tmp = y[i];
+      for (size_type k = 0u; k < n; ++k) x[k] += tmp * Z(k, i);
+    }
+    if (resid <= rtol || flag != SUCCESS) break;
+  }
+  return std::make_tuple(x, flag, iter, num_mv);
+}
+
+#endif  // _HIFIR_DEMOGMRES_HPP
